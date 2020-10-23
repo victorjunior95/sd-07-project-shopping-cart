fa815e4e167b24201a35dfb595901e437272c03a
Mauricio Hamaji Homma <43273469+mhamaji@users.noreply.github.com>
2020-10-23 16:37:56 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 window.onload = function onload() { };
 
+const testing;
+
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
