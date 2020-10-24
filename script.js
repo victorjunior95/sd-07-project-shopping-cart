435dc6c5fa57bc5a99089b8f6313321d947fac37
rafaelmguimaraes <rafaelmguimaraes@gmail.com>
2020-10-23 21:55:40 -0300

    Requisito 02

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-window.onload = function onload() { 
-  const SEARCH_TERM = 'computador';
-  fillProductListByTerm(SEARCH_TERM);
-};
-
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -10,10 +5,72 @@ function createProductImageElement(imageSource) {
   return img;
 }
 
+function getSkuFromProductItem(item) {
+  return item.querySelector('span.item__sku').innerText;
+}
+
+function cartItemClickListener(event) {
+  
+}
+
+function createCartItemElement({ sku, name, salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
+}
+
+const showAlert = (message) => {
+  window.alert(message);
+};
+
+const fetchMLProductListByTerm = async (term) => {
+  const API_URL_ML_SEARCH = `https://api.mercadolibre.com/sites/MLB/search?q=${term}`;
+  let productList;
+  try {
+    const response = await fetch(API_URL_ML_SEARCH);
+    const jsonObject = await response.json();
+    const { results } = jsonObject;
+    productList = results;
+  } catch (error) {
+    showAlert(error);
+  }
+  return productList;
+};
+
+const fetchMLProductItem = async (productId) => {
+  const API_URL_ML_SEARCH_ITEM = `https://api.mercadolibre.com/items/${productId}`;
+  let productItem;
+  try {
+    const response = await fetch(API_URL_ML_SEARCH_ITEM);
+    const jsonObject = await response.json();
+    productItem = jsonObject;
+  } catch (error) {
+    showAlert(error);
+  }
+  return productItem;
+};
+
+
+const productItemClickListener = async (event) => {
+  const productId = getSkuFromProductItem(event.target.parentNode);
+  try {
+    const productItem = await fetchMLProductItem(productId);
+    const { id: sku, title: name, price: salePrice } = productItem;
+    const productLi = createCartItemElement({ sku, name, salePrice });
+    const cartItems = document.querySelector('.cart__items');
+    cartItems.appendChild(productLi);
+  } catch (error) {
+    showAlert(error);
+  }
+};
+
 function createCustomElement(element, className, innerText) {
   const e = document.createElement(element);
   e.className = className;
   e.innerText = innerText;
+  e.addEventListener('click', productItemClickListener);
   return e;
 }
 
@@ -29,48 +86,21 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
-
-function cartItemClickListener(event) {
-  // coloque seu código aqui
-}
-
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
-
-const showAlert = (message) => {
-  window.alert(message);
-}
-
-const fetchMLProductListByTerm = async (term) => {
-  const API_URL_ML_SEARCH = `https://api.mercadolibre.com/sites/MLB/search?q=${term}`;
-  try {
-    const response = await fetch(API_URL_ML_SEARCH);
-    const jsonObject = await response.json(); 
-    const { results } = jsonObject;
-    return results;
-  } catch (error) {
-    showAlert(error);
-  }
-}
-
 const fillProductListByTerm = async (term) => {
   try {
     const productList = await fetchMLProductListByTerm(term);
     productList
-      .map(({id, title, thumbnail}) => ({ sku: id, name: title, image: thumbnail}))
-      .forEach(product => {
-        const sectionItems = document.querySelector(".items");
+      .map(({ id, title, thumbnail }) => ({ sku: id, name: title, image: thumbnail }))
+      .forEach((product) => {
+        const sectionItems = document.querySelector('.items');
         sectionItems.appendChild(createProductItemElement(product));
       });
   } catch (error) {
     showAlert(error);
   }
-}
+};
+
+window.onload = function onload() {
+  const SEARCH_TERM = 'computador';
+  fillProductListByTerm(SEARCH_TERM);
+};
