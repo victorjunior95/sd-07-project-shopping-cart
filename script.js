966a52906d55e50530f9ad4445119d2913e4ab8e
Thadeu Castelo Branco Ramos <thadeucbr@gmail.com>
2020-10-24 14:40:13 -0300

    Fetch feito e funcionando, produtos criados com sucesso

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-window.onload = function onload() { };
-
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -14,10 +12,9 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement({ sku, name, image }) {
+function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   const section = document.createElement('section');
   section.className = 'item';
-
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
@@ -26,18 +23,36 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
-function getSkuFromProductItem(item) {
+/* function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
-}
+} */
 
-function cartItemClickListener(event) {
-  // coloque seu código aqui
-}
+/* function cartItemClickListener(event) {
 
-function createCartItemElement({ sku, name, salePrice }) {
+} */
+
+/* function createCartItemElement({ id: sku, title: name, price: salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
-}
+} */
+
+const searchProduct = (product, maxResults) => `https://api.mercadolibre.com/sites/MLB/search?q=${product}&limit=${maxResults}`;
+
+const productHandler = (productArray) => {
+  const product = Object.entries(productArray);
+  product.forEach(entry => document.querySelector('.items').appendChild(createProductItemElement(entry[1])));
+};
+
+const fetchProductsApi = () => {
+  const apiUrl = searchProduct('computador', 4);
+  fetch(apiUrl)
+  .then(product => product.json())
+  .then(object => productHandler(object.results));
+};
+
+window.onload = function onload() {
+  fetchProductsApi();
+};
