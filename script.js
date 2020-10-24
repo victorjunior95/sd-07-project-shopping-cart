88c3b80deb581f662374e51e20741a18ddb61e33
Berilo Wanderley <wberilo@gmail.com>
2020-10-24 17:15:27 -0300

    cc

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//window.onload = function onload() {};
+window.onload = function onload() {};
 
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
@@ -14,36 +14,11 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-const grabItems = async api => {
-  const items = await fetch(api)
-    .then(r => r.json())
-    .then(r => r.results);
-  itemsToSection(items);
-};
-const addToCart = async event => {
-  const id = event.target.parentNode.childNodes[0].innerText;
-  const item = await fetch(`https://api.mercadolibre.com/items/${id}`).then(r => r.json());
-  console.log(item);
-  product = { sku: item.id, name: item.title, salePrice: item.price };
-  const cartElement = createCartItemElement(product);
-  cartElement.addEventListener('click', event => event.target.remove());
-  appendSectionOnto(cartElement, 'ol.cart__items');
-};
-
 const appendSectionOnto = (section, onto) => document.querySelector(onto).appendChild(section);
 const handleAddToCart = () =>
   document.querySelectorAll('.item__add').forEach(element => {
     element.addEventListener('click', addToCart);
   });
-
-const itemsToSection = items => {
-  items.forEach(({ id, title, thumbnail }) => {
-    const product = { sku: id, name: title, image: thumbnail };
-    const section = createProductItemElement(product);
-    appendSectionOnto(section, 'section.items');
-  });
-  handleAddToCart();
-};
 
 function createProductItemElement({ sku, name, image }) {
   const section = document.createElement('section');
@@ -72,5 +47,30 @@ function createCartItemElement({ sku, name, salePrice }) {
   li.addEventListener('click', cartItemClickListener);
   return li;
 }
+
+const itemsToSection = items => {
+  items.forEach(({ id, title, thumbnail }) => {
+    const product = { sku: id, name: title, image: thumbnail };
+    const section = createProductItemElement(product);
+    appendSectionOnto(section, 'section.items');
+  });
+  handleAddToCart();
+};
+
+const addToCart = async event => {
+  const id = event.target.parentNode.childNodes[0].innerText;
+  const item = await fetch(`https://api.mercadolibre.com/items/${id}`).then(r => r.json());
+  console.log(item);
+  product = { sku: item.id, name: item.title, salePrice: item.price };
+  const cartElement = createCartItemElement(product);
+  cartElement.addEventListener('click', event => event.target.remove());
+  appendSectionOnto(cartElement, 'ol.cart__items');
+};
+const grabItems = async (api) => {
+  const items = await fetch(api)
+    .then(r => r.json())
+    .then(r => r.results);
+  itemsToSection(items);
+};
 
 grabItems('https://api.mercadolibre.com/sites/MLB/search?q=computador');
