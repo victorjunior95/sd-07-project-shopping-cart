55a2cf6a2b7d6d345bed477e7e28fd6df7939b1a
Vitor Rodrigues <vitor_rc@outlook.com>
2020-10-23 22:20:38 -0300

    Requisitos 2 e 3

--- script.js
+++ script.js
@@ -24,21 +24,37 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
-// function getSkuFromProductItem(item) {
-//   return item.querySelector('span.item__sku').innerText;
-// }
+function getSkuFromProductItem(item) {
+  return item.querySelector('span.item__sku').innerText;
+}
 
-// function cartItemClickListener(event) {
-//   // coloque seu código aqui
-// }
+function cartItemClickListener(event) {
+  event.target.remove();
+}
 
-// function createCartItemElement({ sku, name, salePrice }) {
-//   const li = document.createElement('li');
-//   li.className = 'cart__item';
-//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-//   li.addEventListener('click', cartItemClickListener);
-//   return li;
-// }
+function createCartItemElement({ sku, name, salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
+}
+
+async function addToCart(productAdd) {
+  const itemID = getSkuFromProductItem(productAdd.target.parentElement);
+  const endpoint = `https://api.mercadolibre.com/items/${itemID}`;
+  try {
+    const response = await fetch(endpoint);
+    const data = await response.json();
+    const { id: sku, title: name, price: salePrice } = data;
+    const productComponents = { sku, name, salePrice };
+    const cartItems = document.querySelector('.cart__items');
+    cartItems.appendChild(createCartItemElement(productComponents));
+    console.log(productComponents);
+  } catch (error) {
+    console.error(error);
+  }
+}
 
 async function fetchItems() {
   const endpoint = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
@@ -54,7 +70,7 @@ async function fetchItems() {
         const productComponents = { sku, name, image };
         const items = document.querySelector('.items');
         const product = createProductItemElement(productComponents);
-
+        product.querySelector('button.item__add').addEventListener('click', addToCart);
         items.appendChild(product);
       }
     }
