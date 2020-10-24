61430a2ae11f2b0833c85bbec056fb178ca44541
Berilo Wanderley <wberilo@gmail.com>
2020-10-24 17:21:19 -0300

    cc

--- script.js
+++ script.js
@@ -44,6 +44,19 @@ function createCartItemElement({ sku, name, salePrice }) {
   return li;
 }
 
+const addToCart = async (event) => {
+  const id = event.target.parentNode.childNodes[0].innerText;
+  const item = await fetch(`https://api.mercadolibre.com/items/${id}`).then(r => r.json());
+  console.log(item);
+  product = { sku: item.id, name: item.title, salePrice: item.price };
+  const cartElement = createCartItemElement(product);
+  cartElement.addEventListener('click', clicked => clicked.target.remove());
+  appendSectionOnto(cartElement, 'ol.cart__items');
+};
+
+const handleAddToCart = () =>
+  document.querySelectorAll('.item__add').forEach(element => element.addEventListener('click', addToCart));
+
 const itemsToSection = (items) => {
   items.forEach(({ id, title, thumbnail }) => {
     const product = { sku: id, name: title, image: thumbnail };
@@ -53,23 +66,11 @@ const itemsToSection = (items) => {
   handleAddToCart();
 };
 
-const addToCart = async (event) => {
-  const id = event.target.parentNode.childNodes[0].innerText;
-  const item = await fetch(`https://api.mercadolibre.com/items/${id}`).then(r => r.json());
-  console.log(item);
-  product = { sku: item.id, name: item.title, salePrice: item.price };
-  const cartElement = createCartItemElement(product);
-  cartElement.addEventListener('click', event => event.target.remove());
-  appendSectionOnto(cartElement, 'ol.cart__items');
-};
 const grabItems = async (api) => {
   const items = await fetch(api)
     .then(r => r.json())
     .then(r => r.results);
   itemsToSection(items);
 };
-
-const handleAddToCart = () =>
-  document.querySelectorAll('.item__add').forEach(element => element.addEventListener('click', addToCart));
 
 grabItems('https://api.mercadolibre.com/sites/MLB/search?q=computador');
