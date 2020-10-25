f686b3a5c7cc85a92e013815271614459b0e117a
danielmlduarte <danielduarte.tryber@gmail.com>
2020-10-25 03:04:02 -0300

    Adicionando funçao de loading para requisiçoes de API - Refatorando

--- script.js
+++ script.js
@@ -108,7 +108,7 @@ const loadingTextOnScreen = (isLoading) => {
   const itensSection = document.querySelector('.items');
   if (isLoading) {
     const loadingElement = document.createElement('p');
-    loadingElement.className = '.loading';
+    loadingElement.className = 'loading';
     loadingElement.innerText = 'loading...';
     itensSection.appendChild(loadingElement);
   } else {
