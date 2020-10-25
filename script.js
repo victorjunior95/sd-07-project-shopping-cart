dc760469809b13faf38b4abe0ee1e996eadd910e
Berilo Wanderley <wberilo@gmail.com>
2020-10-25 16:09:37 -0300

    localstorage save cart completed

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let cartItems = []
+
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -41,19 +43,23 @@ function createCartItemElement({ sku, name, salePrice }) {
   li.addEventListener('click', cartItemClickListener);
   return li;
 }
-
-const addToCart = async (event) => {
-  const id = event.target.parentNode.childNodes[0].innerText;
+const updateCart = () => localStorage.setItem('cart',JSON.stringify(cartItems))
+const addToCart = async (id) => {
   const item = await fetch(`https://api.mercadolibre.com/items/${id}`).then(r => r.json());
-  console.log(item);
   product = { sku: item.id, name: item.title, salePrice: item.price };
   const cartElement = createCartItemElement(product);
-  cartElement.addEventListener('click', clicked => clicked.target.remove());
+  cartElement.addEventListener('click', (clicked) => {
+    const id = clicked.target.innerText.split(' ')[1];
+    cartItems.splice(cartItems.indexOf(id),1)
+    updateCart()
+    clicked.target.remove()});
+  cartItems.push(id)
+  updateCart()
   appendSectionOnto(cartElement, 'ol.cart__items');
 };
-
+const getIdAndSendToCart = (event)=> addToCart(event.target.parentNode.childNodes[0].innerText)
 const handleAddToCart = () =>
-  document.querySelectorAll('.item__add').forEach(element => element.addEventListener('click', addToCart));
+  document.querySelectorAll('.item__add').forEach(element => element.addEventListener('click', getIdAndSendToCart));
 
 const itemsToSection = (items) => {
   items.forEach(({ id, title, thumbnail }) => {
@@ -71,6 +77,11 @@ const grabItems = async (api) => {
   itemsToSection(items);
 };
 
+function populateWithStorage (items) {
+  items.forEach(addToCart);
+}
+
 window.onload = function onload() {
+  populateWithStorage(JSON.parse(localStorage.getItem('cart')))
   grabItems('https://api.mercadolibre.com/sites/MLB/search?q=computador');
 };
