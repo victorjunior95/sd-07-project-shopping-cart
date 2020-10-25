49c60a7bb978c13d2480066cb5637a28197dfee0
Elisa <elisalibanio1@gmail.comgit config --global user.email elisalibanio1@gmail.com>
2020-10-25 10:14:16 -0300

    enviando projeto

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-window.onload = function onload() { };
-
-const  createProductImageElement  = (imageSource) {
+function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
   img.src = imageSource;
+
   return img;
 }
 
@@ -13,8 +12,9 @@ function createCustomElement(element, className, innerText) {
   e.innerText = innerText;
   return e;
 }
+// essa função cria a estrutura de hmtl dos produtos: img,title...
 
-function createProductItemElement({ sku, name, image }) {
+function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   const section = document.createElement('section');
   section.className = 'item';
 
@@ -26,18 +26,110 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
+
+
+function setLocalStorage() {
+  const listSalve = document.querySelector('.cart__items');
+  localStorage.setItem('listSalve', listSalve.innerHTML);
+  console.log(listSalve.innerHTML);
+}
+
+function getLocalStorage() {
+  const listSalve = document.querySelector('.cart__items');
+  listSalve.innerHTML = localStorage.getItem('listSalve');
 }
 
 function cartItemClickListener(event) {
-  // coloque seu código aqui
+  event.addEventListener('click', () => {
+    const list = document.querySelector('.cart__items');
+    list.removeChild(event);
+    setLocalStorage();
+  });
 }
 
-function createCartItemElement({ sku, name, salePrice }) {
+function emptyCart() {
+  const buttonClearList = document.querySelector('.empty-cart');
+  const cartItems = document.querySelector('.cart__items');
+  buttonClearList.addEventListener('click', () => {
+    cartItems.innerHTML = '';
+  });
+}
+
+function sumItems(price) {
+let priceItems = document.querySelector('.cart__item')
+console.log(priceItems)
+  
+
+  
+}
+
+function createCartItemElement({ id: sku, title: name, price: salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
+  li.addEventListener('click', cartItemClickListener(li));
+  sumItems(salePrice);
   return li;
 }
+
+function clickButton(event) {
+  const itemId = event.path[1].childNodes[0].innerHTML;
+  const endpoint = `https://api.mercadolibre.com/items/${itemId}`;
+  fetch(endpoint)
+    .then(response => response.json())
+    .then((object) => {
+      const cartItem = createCartItemElement(object);
+      const ol = document.querySelector('.cart__items');
+      ol.appendChild(cartItem);
+      console.log(cartItem)
+      setLocalStorage();
+    });
+}
+
+function functionRenderItem(arrayProducts) {
+  arrayProducts.forEach((element) => {
+    const newItem = createProductItemElement(element);
+    const section = document.querySelector('.items');
+    section.appendChild(newItem).addEventListener('click', clickButton);
+  });
+}
+function addCartCachedItemsAListener() {
+  const cachedList = document.querySelectorAll('.cart__item');
+  cachedList.forEach((element) => {
+    cartItemClickListener(element);
+  });
+}
+const query = 'computador';
+const apiInfo = {
+  api: 'https://api.mercadolibre.com/',
+  endpoint: 'sites/MLB/search?q=',
+};
+
+const url = `${apiInfo.api}${apiInfo.endpoint}${query}`;
+
+function loadingFetch() {
+  const element = document.createElement('p');
+  element.classList.add('loading');
+  element.innerHTML = 'Loading...';
+  document.querySelector('.container').appendChild(element);
+}
+
+const fecthItems = () => {
+  fetch(url)
+    .then(response => response.json())
+    .then((object) => {
+      const resultsArray = object.results;
+      functionRenderItem(resultsArray);
+    })
+    .then(() => {
+      const element = document.querySelector('.loading');
+      document.querySelector('.container').removeChild(element);
+    });
+};
+window.onload = function onload() {
+  loadingFetch();
+  fecthItems(url);
+  emptyCart();
+  getLocalStorage();
+  addCartCachedItemsAListener();
+};
