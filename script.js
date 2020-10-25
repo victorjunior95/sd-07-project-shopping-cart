65562acca5a92e007bd12b0ea5dc8d819dcefdc8
moacyrrln <moacyrrln@gmail.com>
2020-10-24 22:26:56 -0300

    201024222656

--- script.js
+++ script.js
@@ -1,62 +1,61 @@
-window.onload = function onload() {
+function createProductImageElement(imageSource) {
+  const img = document.createElement('img');
+  img.className = 'item__image';
+  img.src = imageSource;
+  return img;
+}
 
-  function createProductImageElement(imageSource) {
-    const img = document.createElement('img');
-    img.className = 'item__image';
-    img.src = imageSource;
-    return img;
-  }
-  
-  function createCustomElement(element, className, innerText) {
-    const e = document.createElement(element);
-    e.className = className;
-    e.innerText = innerText;
-    return e;
-  }
-  
-  function createProductItemElement( sku, name, image ) {
-    const section = document.createElement('section');
-    section.className = 'item';
-  
-    section.appendChild(createCustomElement('span', 'item__sku', sku));
-    section.appendChild(createCustomElement('span', 'item__title', name));
-    section.appendChild(createProductImageElement(image));
-    section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-  
-    return section;
-  }
-  
-  function getSkuFromProductItem(item) {
-    return item.querySelector('span.item__sku').innerText;
-  }
-  
-  function cartItemClickListener(event) {
-    // coloque seu código aqui
-  }
-  
-  function createCartItemElement({ sku, name, salePrice }) {
-    const li = document.createElement('li');
-    li.className = 'cart__item';
-    li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-    li.addEventListener('click', cartItemClickListener);
-    return li;
-  }
-  const fetchItens = async () => {
-    const endpoint = `https://api.mercadolibre.com/sites/MLB/search?q=computador`;
-    try {
-      const response = await fetch(endpoint);
-      const object = await response.json();
-      if (object.error) {
-        throw new Error(object.error);
-      } else {
-        for (let i = 0; i < 8; i += 1) {
-          document.getElementsByClassName("items")[0].appendChild(createProductItemElement( object.results[i].id, object.results[i].title, object.results[i].thumbnail ))
-        }     
+function createCustomElement(element, className, innerText) {
+  const e = document.createElement(element);
+  e.className = className;
+  e.innerText = innerText;
+  return e;
+}
+
+function createProductItemElement(sku, name, image) {
+  const section = document.createElement('section');
+  section.className = 'item';
+
+  section.appendChild(createCustomElement('span', 'item__sku', sku));
+  section.appendChild(createCustomElement('span', 'item__title', name));
+  section.appendChild(createProductImageElement(image));
+  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+  return section;
+}
+
+// function getSkuFromProductItem(item) {
+//   return item.querySelector('span.item__sku').innerText;
+// } cc
+
+// function cartItemClickListener(event) {
+//   // coloque seu código aqui
+// } cc
+
+// function createCartItemElement({ sku, name, salePrice }) {
+//   const li = document.createElement('li');
+//   li.className = 'cart__item';
+//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+//   li.addEventListener('click', cartItemClickListener);
+//   return li;
+// } cc
+
+const fetchItens = async () => {
+  const endpoint = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+  try {
+    const response = await fetch(endpoint);
+    const object = await response.json();
+    if (object.error) {
+      throw new Error(object.error);
+    } else {
+      for (let i = 0; i < 8; i += 1) {
+        document.getElementsByClassName('items')[0].appendChild(createProductItemElement(object.results[i].id, object.results[i].title, object.results[i].thumbnail));
       }
-    } catch (error) {
-      showAlert(error);
     }
+  } catch (error) {
+    showAlert(error);
   }
-  fetchItens();
- };
+};
 
+window.onload = function onload() {
+  fetchItens();
+};
