9d116738659fb0bb1a3e84f88b500366b3523587
ygorvmdf <ygorvitor.fonseca@gmail.com>
2020-10-25 11:39:27 -0300

    iniciando projeto carrinho de comprar | primeiro commit

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement({ sku, name, image }) {
+function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   const section = document.createElement('section');
   section.className = 'item';
 
@@ -26,18 +26,32 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
+// function getSkuFromProductItem(item) {
+//   return item.querySelector('span.item__sku').innerText;
+// }
 
-function cartItemClickListener(event) {
-  // coloque seu código aqui
-}
+// function cartItemClickListener(event) {
+//   // coloque seu código aqui
+// }
 
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
+// function createCartItemElement({ sku, name, salePrice }) {
+//   const li = document.createElement('li');
+//   li.className = 'cart__item';
+//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+//   li.addEventListener('click', cartItemClickListener);
+//   return li;
+// }
+
+const fecthProductList = (product) => {
+  const productQuery = `https://api.mercadolibre.com/sites/MLB/search?q=${product}`;
+
+  fetch(productQuery)
+  .then(response => response.json())
+  .then(object => object.results)
+  .then(result => result.forEach((item) => {
+    const section = createProductItemElement(item);
+    document.getElementsByClassName('items')[0].appendChild(section);
+  }));
+};
+
+fecthProductList('computer');
