653743e5cf57238d5dcd0df73968f864003e1729
Rogerio-feitor <roger.feitor@gmail.com>
2020-10-25 18:34:24 -0300

    Resolvendo a requisito 1, implementando os produtos da API na tela

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 window.onload = function onload() { };
-
+const showAlert = (message) => {
+  window.alert(message);
+};
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -14,30 +16,51 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement({ sku, name, image }) {
+function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   const section = document.createElement('section');
   section.className = 'item';
-
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!', sku));
 
   return section;
 }
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
+const productOnScreen = (vector) => {
+  const items = document.querySelector('.items');
+  vector.forEach(item => items.appendChild(createProductItemElement(item)));
+};
+const fecthComputerAsyncAwait = async () => {
+  const endpoints = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+  try {
+    const response = await fetch(endpoints);
+    const objekt = await response.json();
+    if (objekt.error) {
+      throw new Error(objekt.error);
+    } else {
+      productOnScreen(objekt.results);
+    }
+  } catch (error) {
+    showAlert(error);
+  }
+};
 
-function cartItemClickListener(event) {
+// function getSkuFromProductItem(item) {
+//   return item.querySelector('span.item__sku').innerText;
+// }
+
+// function cartItemClickListener(event) {
   // coloque seu código aqui
-}
+//  }
 
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
+// function createCartItemElement({ sku, name, salePrice }) {
+//   const li = document.createElement('li');
+//   li.className = 'cart__item';
+//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+//   li.addEventListener('click', cartItemClickListener);
+//   return li;
+// }
+
+
+fecthComputerAsyncAwait();
