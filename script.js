f34da85e336cbc522a687509ffcc59fdbcbc00af
kramersilva <kramerscs@gmail.com>
2020-10-26 17:10:36 -0300

    soma itens

--- script.js
+++ script.js
@@ -60,7 +60,7 @@ async function updatePrice() {
   }
 }
 
-function storeCart() {
+async function storeCart() {
   localStorage.setItem('cart', cart.innerHTML);
   localStorage.setItem('totalPrice', sumItem);
   updatePrice();
