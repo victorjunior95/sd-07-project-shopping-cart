fe207a3290a1272bb5c986f4561f428ae9713e55
Rogerio-feitor <roger.feitor@gmail.com>
2020-10-26 17:26:01 -0300

    Resolvendo o requisito 2, usando async e await nessa  window.onload = async function onload().

--- script.js
+++ script.js
@@ -8,10 +8,13 @@ function createProductImageElement(imageSource) {
   return img;
 }
 
-function createCustomElement(element, className, innerText) {
+function createCustomElement(element, className, innerText, sku = null) {
   const e = document.createElement(element);
   e.className = className;
   e.innerText = innerText;
+  if (element === 'button') {
+    e.dataset.id = sku;
+  }
   return e;
 }
 
@@ -45,22 +48,53 @@ const fecthComputerAsyncAwait = async () => {
   }
 };
 
+function cartItemClickListener() {
+  const cartItems = document.getElementsByClassName('cart__items');
+  return cartItems;
+}
+
+function createCartItemElement({ id: sku, title: name, price: salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
+}
+
+function addCartListById(elementId) {
+  const cartItems = document.querySelector('.cart__items');
+  cartItems.appendChild(createCartItemElement(elementId));
+}
+
+const fecthIdAsyncAwait = async (id) => {
+  const endpoint = `https://api.mercadolibre.com/items/${id}`;
+  try {
+    const response = await fetch(endpoint);
+    const object = await response.json();
+    if (object.error) {
+      throw new Error(object.error);
+    } else {
+      addCartListById(object);
+    }
+  } catch (error) {
+    showAlert(error);
+  }
+};
 // function getSkuFromProductItem(item) {
 //   return item.querySelector('span.item__sku').innerText;
 // }
 
-// function cartItemClickListener(event) {
-  // coloque seu código aqui
-//  }
+function catchAllProductEvent() {
+  const addCart = document.querySelectorAll('.item__add');
+  addCart.forEach((element) => {
+    element.addEventListener('click', (event) => {
+      const idSku = event.target.dataset.id;
+      fecthIdAsyncAwait(idSku);
+    });
+  });
+}
 
-// function createCartItemElement({ sku, name, salePrice }) {
-//   const li = document.createElement('li');
-//   li.className = 'cart__item';
-//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-//   li.addEventListener('click', cartItemClickListener);
-//   return li;
-// }
-
-window.onload = function onload() {
-  fecthComputerAsyncAwait();
+window.onload = async function onload() {
+  await fecthComputerAsyncAwait();
+  catchAllProductEvent();
 };
