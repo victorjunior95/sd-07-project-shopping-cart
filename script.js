47207faaf3bce118fe5cf8ad0f8794cabd32c7e4
Galaraz <jvictor.si.2013@gmail.com>
2020-10-26 15:15:30 -0300

    VQV

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 window.onload = function onload() { };
-
+/* https://api.mercadolibre.com/sites/MLB/search?q=$computador */
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
