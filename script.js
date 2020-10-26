a24433f65f2549e436c21d81a056d15153d43fa1
Leonardo <lecastro16@gmail.com>
2020-10-26 16:33:42 -0300

    Leonardo Sardinha - Iniciando o Projeto Shopping Cart

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 window.onload = function onload() { };
-
+// Iniciando o projeto
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
