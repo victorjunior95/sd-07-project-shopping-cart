c70a421d2d91c57b71859e2ddc16b11f6e023bb1
TIAGO <tiagoesdrasvieira@gmail.com>
2020-10-26 15:48:29 -0300

    Desafio 1

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement({ sku, name, image }) {
+function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   const section = document.createElement('section');
   section.className = 'item';
 
@@ -26,18 +26,40 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
-function getSkuFromProductItem(item) {
+/* function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
-}
+}*/
 
-function cartItemClickListener(event) {
+/* function cartItemClickListener(event) {
   // coloque seu código aqui
-}
+}*/
 
-function createCartItemElement({ sku, name, salePrice }) {
+function createCartItemElement({ id: sku, title: name, price: salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
 }
+
+const showAlert = (message) => {
+  window.alert(message);
+};
+const fetchList = async () => {
+  const endpoint = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+  try {
+    const response = await fetch(endpoint);
+    const object = await response.json();
+    if (object.error) {
+      throw new Error('Um erro ocorreu!');
+    } else {
+      object.results.forEach((result) => {
+        createCartItemElement(result);
+        createProductItemElement(result);
+      });
+    }
+  } catch (error) {
+    showAlert(error);
+  }
+};
+fetchList();
