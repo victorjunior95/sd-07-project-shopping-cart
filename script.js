2a668d0e3fe544c31658fecc29503c4fe2563b77
moacyrrln <moacyrrln@gmail.com>
2020-10-26 14:19:42 -0300

    201026141942

--- script.js
+++ script.js
@@ -78,7 +78,7 @@ function removeloading() {
 
 const fetchItens = async () => {
   createloading();
-  const endpoint = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+  const endpoint = 'https://api.mercadolibre.com/sites/MLB/search?q=modem';
   try {
     const response = await fetch(endpoint);
     const object = await response.json();
