122ab7575700f2060e9fd35af44fbc663dc0d994
alvaro <alvarovascon2@gmail.com>
2020-10-26 16:08:10 -0300

    addressing CC issues- 2

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-function fetchProductApi(query) {
-  const endpoint = `https://api.mercadolibre.com/sites/MLB/search?q=${query}`;
-
-  fetch(endpoint)
-    .then(response => response.json())
-    .then(object => object.results.forEach(({ id, title, thumbnail }) => {
-      const newItem = { sku: id, name: title, image: thumbnail };
-      const HtmlElement = createProductItemElement(newItem);
-      const section = document.querySelector('.items');
-      section.appendChild(HtmlElement);
-    }));
-}
-
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -51,6 +38,19 @@ function createCartItemElement({ sku, name, salePrice }) {
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
+}
+
+function fetchProductApi(query) {
+  const endpoint = `https://api.mercadolibre.com/sites/MLB/search?q=${query}`;
+
+  fetch(endpoint)
+    .then(response => response.json())
+    .then(object => object.results.forEach(({ id, title, thumbnail }) => {
+      const newItem = { sku: id, name: title, image: thumbnail };
+      const HtmlElement = createProductItemElement(newItem);
+      const section = document.querySelector('.items');
+      section.appendChild(HtmlElement);
+    }));
 }
 
 window.onload = function onload() {
