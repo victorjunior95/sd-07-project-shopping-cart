a1d69b269e10dc92673000af5f978698efc80b47
Carol Bezerra <carol.arbe@gmail.com>
2020-10-26 16:46:54 -0300

    Requisito 1 com CC

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-window.onload = function onload() { 
-  functionToFetch();
-};
-
-function createProductImageElement(imageSource) {
-  const img = document.createElement('img');
-  img.className = 'item__image';
-  img.src = imageSource;
-  return img;
-}
-
 function createCustomElement(element, className, innerText) {
   const e = document.createElement(element);
   e.className = className;
@@ -16,7 +5,7 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement({ id:sku, title:name, thumbnail:image }) {
+function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   const section = document.createElement('section');
   section.className = 'item';
 
@@ -28,32 +17,43 @@ function createProductItemElement({ id:sku, title:name, thumbnail:image }) {
   return section;
 }
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
-
-function cartItemClickListener(event) {
-  // coloque seu código aqui
-}
-
-function createCartItemElement({ id:sku, title:name, price:salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
-
 const selectChild = (arrayValue) => { // referência Rodrigo Sudario
   const sectionHere = document.querySelector('.items'); // section que vai receber o novo item
   const value = Object.entries(arrayValue); // para formar arrays com chave e valor
-  value.forEach((newItem) => sectionHere.appendChild(createProductItemElement(newItem[1])));
+  value.forEach(newItem => sectionHere.appendChild(createProductItemElement(newItem[1])));
   // para que a section receba cada um dos objetos como filhos
-}
+};
 
 const functionToFetch = () => {
   const endpoint = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
   fetch(endpoint)
     .then(response => response.json())
-    .then(object => selectChild(object.results)) // envia só o results
-}
+    .then(object => selectChild(object.results)); // envia só o results
+};
+
+window.onload = function onload() {
+  functionToFetch();
+};
+
+// function createProductImageElement(imageSource) {
+//   const img = document.createElement('img');
+//   img.className = 'item__image';
+//   img.src = imageSource;
+//   return img;
+// }
+
+// function getSkuFromProductItem(item) {
+//   return item.querySelector('span.item__sku').innerText;
+// }
+
+// function cartItemClickListener(event) {
+//   // coloque seu código aqui
+// }
+
+// function createCartItemElement({ id: sku, title: name, price: salePrice }) {
+//   const li = document.createElement('li');
+//   li.className = 'cart__item';
+//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+//   li.addEventListener('click', cartItemClickListener);
+//   return li;
+// }
