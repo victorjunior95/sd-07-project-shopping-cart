63555b1d03d70127b991811ed3a0682b2a5bff9d
Arthur Massaini de Santana <massaini.arthur@gmail.com>
2020-10-26 15:44:42 -0300

    Arrumando CC requisito 2

--- script.js
+++ script.js
@@ -26,6 +26,22 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
+function getSkuFromProductItem(item) {
+  return item.querySelector('span.item__sku').innerText;
+}
+
+function cartItemClickListener(event) {
+  // coloque seu código aqui
+}
+
+function createCartItemElement({ sku, name, salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
+}
+
 //-----------------------------------------------------------------------
 
 const putOnCart = (data) => {
@@ -81,22 +97,6 @@ const fetchMercado = () => {
 };
 
 //-----------------------------------------------------------------------
-
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
-
-function cartItemClickListener(event) {
-  // coloque seu código aqui
-}
-
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
 
 window.onload = async function onload() {
   fetchMercado();
