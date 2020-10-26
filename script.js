1940af20b756f4a65cefad25cbcc8e4b466b5181
Quintiliano Dalete <quintilianodalete@gmail.com>
2020-10-26 12:07:44 -0300

    iniciando o projeto. VAMOS COM TUDO :rocket:

--- script.js
+++ script.js
@@ -31,7 +31,9 @@ function getSkuFromProductItem(item) {
 }
 
 function cartItemClickListener(event) {
+  
   // coloque seu código aqui
+
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
