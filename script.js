2417b90a072674c9cda8e445efcb0aa1b6efa2ba
Lenodeoliveira <lenodeoliveira@gmail.com>
2020-10-26 17:26:43 -0300

    Adicionado requisito 2

--- script.js
+++ script.js
@@ -5,6 +5,25 @@ function createProductImageElement(imageSource) {
   return img;
 }
 
+function createCartItemElement({ sku, name, salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+
+  return li;
+}
+
+const addItemCart = async ({ sku }) => {
+  const item = await fetch(`https://api.mercadolibre.com/items/${sku}`)
+  .then(data => data.json())
+  .then(obj => createCartItemElement({
+    sku: obj.id,
+    name: obj.title,
+    salePrice: obj.price }),
+  );
+  await document.querySelector('.cart__items').appendChild(item);
+};
+
 function createCustomElement(element, className, innerText) {
   const e = document.createElement(element);
   e.className = className;
@@ -19,8 +38,12 @@ function createProductItemElement({ sku, name, image }) {
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+  const button = createCustomElement('button', 'item__add', 'Adicionar ao carrinho!');
+  button.addEventListener('click', () => {
+    addItemCart({ sku });
+  });
 
+  section.appendChild(button);
   return section;
 }
 
@@ -28,17 +51,8 @@ function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
-function cartItemClickListener(event) {
-  // coloque seu código aqui
-}
-
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
+const cartItemClickListener = (event) => {
+};
 
 const fetchMercadoLivre = () => {
   const API_URL = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
