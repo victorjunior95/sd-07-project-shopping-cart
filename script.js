df5951aa8d92c4d9c639a8a1a2c14b1c3300cfae
Carol Bezerra <carol.arbe@gmail.com>
2020-10-26 16:31:54 -0300

    Requisito 1

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-window.onload = function onload() { };
+window.onload = function onload() { 
+  functionToFetch();
+};
 
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
@@ -14,7 +16,7 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement({ sku, name, image }) {
+function createProductItemElement({ id:sku, title:name, thumbnail:image }) {
   const section = document.createElement('section');
   section.className = 'item';
 
@@ -34,10 +36,24 @@ function cartItemClickListener(event) {
   // coloque seu código aqui
 }
 
-function createCartItemElement({ sku, name, salePrice }) {
+function createCartItemElement({ id:sku, title:name, price:salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
+}
+
+const selectChild = (arrayValue) => { // referência Rodrigo Sudario
+  const sectionHere = document.querySelector('.items'); // section que vai receber o novo item
+  const value = Object.entries(arrayValue); // para formar arrays com chave e valor
+  value.forEach((newItem) => sectionHere.appendChild(createProductItemElement(newItem[1])));
+  // para que a section receba cada um dos objetos como filhos
+}
+
+const functionToFetch = () => {
+  const endpoint = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+  fetch(endpoint)
+    .then(response => response.json())
+    .then(object => selectChild(object.results)) // envia só o results
 }
