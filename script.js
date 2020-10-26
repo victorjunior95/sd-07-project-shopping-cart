28fc255bf363a6ed7379e689a413fa5183298a17
vitorAmorims <vtamorims@gmail.com>
2020-10-26 20:55:33 -0300

    code climate

--- script.js
+++ script.js
@@ -76,6 +76,7 @@ const appendToChart = (item) => {
 
 // requisito 1 - com Async/Await
 const fetchProducts = async () => {
+  loading();
   const endpoint = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
   const response = await fetch(endpoint);
   const object = await response.json();
@@ -96,7 +97,6 @@ function cleanToChart() {
 }
 
 window.onload = function onload() {
-  loading();
   fetchProducts();
   cleanToChart();
 };
