0503c8fdf81517b58be0069ee8f5d0502f23e2c2
Fernando Antunes <fernando.antunes1@gmail.com>
2020-10-26 19:48:22 -0300

    Retirando o divisor ||| do código. Não foi aceito

--- script.js
+++ script.js
@@ -61,7 +61,7 @@ const addLocalStorage = async ({ sku, name, salePrice }) => {
 function createCartItemElement({ sku, name, salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} ||| NAME: ${name} ||| PRICE: $${salePrice}`;
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
 }
