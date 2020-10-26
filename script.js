a969ef720098fab6d326a42e65afd46168c465ed
richardfazeredo <richardcompletetour@gmail.com>
2020-10-26 18:28:57 -0300

    Requirement 1 - codeclimate corrections

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 window.onload = function onload() {};
 
 function createProductImageElement(imageSource) {
-  const img = document.createElement("img");
-  img.className = "item__image";
+  const img = document.createElement('img');
+  img.className = 'item__image';
   img.src = imageSource;
   return img;
 }
@@ -15,40 +15,38 @@ function createCustomElement(element, className, innerText) {
 }
 
 function createProductItemElement({ sku, name, image }) {
-  const section = document.createElement("section");
-  section.className = "item";
+  const section = document.createElement('section');
+  section.className = 'item';
 
-  section.appendChild(createCustomElement("span", "item__sku", sku));
-  section.appendChild(createCustomElement("span", "item__title", name));
+  section.appendChild(createCustomElement('span', 'item__sku', sku));
+  section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
   section.appendChild(
-    createCustomElement("button", "item__add", "Adicionar ao carrinho!")
+    createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'),
   );
 
   return section;
 }
 
-function getSkuFromProductItem(item) {
-  return item.querySelector("span.item__sku").innerText;
-}
+// function getSkuFromProductItem(item) {
+//   return item.querySelector('span.item__sku').innerText;
+// }
 
 function addItemsGotFromQuery(data) {
-  const itemsSection = document.getElementById("items");
-  const item = data.map(({ id, title, thumbnail }) => {
+  const itemsSection = document.getElementById('items');
+  const item = data.forEach(({ id, title, thumbnail }) => {
     itemsSection.appendChild(createProductItemElement({
-       sku: id, name: title, image: thumbnail 
-      })
-    )
-    return item;
-    }
-  );
+      sku: id, name: title, image: thumbnail,
+    }));
+  });
+  return item;
 }
 
-function cartItemClickListener(event) {
-  const param = { headers: { Accept: "application/json" } };
+function cartItemClickListener() {
+  const param = { headers: { Accept: 'application/json' } };
   fetch(
-    "https://api.mercadolibre.com/sites/MLB/search?q=computador",
-    param
+    'https://api.mercadolibre.com/sites/MLB/search?q=computador',
+    param,
   ).then((response) => {
     response.json().then((data) => {
       const results = data.results;
@@ -59,10 +57,10 @@ function cartItemClickListener(event) {
 
 cartItemClickListener();
 
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement("li");
-  li.className = "cart__item";
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener("click", cartItemClickListener);
-  return li;
-}
+// function createCartItemElement({ sku, name, salePrice }) {
+//   const li = document.createElement("li");
+//   li.className = "cart__item";
+//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+//   li.addEventListener("click", cartItemClickListener);
+//   return li;
+// }
