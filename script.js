4e167909199f29f7c3367caf6da6fe5b8a909a74
André Ferreira <andrefpires1@gmail.com>
2020-10-26 15:44:09 -0300

    iniciando o projeto: VAMOS COM TUDO :rocket:

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ function getSkuFromProductItem(item) {
 }
 
 function cartItemClickListener(event) {
-  // coloque seu código aqui
+  // iniciando o projeto: VAMOS COM TUDO :rocket:
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
