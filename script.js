c7a6e1d6d17b92c293b0a7ae066c3f134d5588fc
marcelo ivan <sagacello@hotmail.com>
2020-10-26 15:46:52 -0300

    primeira tentativa corrigindo cc

--- script.js
+++ script.js
@@ -1,46 +1,7 @@
-const converteObjetosDesejados = (objetosDoResult) => {
-  console.log(objetosDoResult);
-  objetosDoResult.map(item => {
-    const cadaProduto = {
-      sku: item.id,
-      name: item.title,
-      image: item.thumbnail,
-    };
-    return inserirItemHtml(cadaProduto);
-  });
-}
-
-const CarregaProdutos = () => {
-  const endpoint = 'https://api.mercadolibre.com/sites/MLB/search?q=$computador';
-  // não posso transformar direto em json porque tem que esperar respostas do servidor
-  fetch(endpoint)
-    .then(resposta => resposta.json()) // endpoint convertido em json
-    .then(objeto => { // dentro do objeto eu busquei os results
-      converteObjetosDesejados(objeto.results);
-    })
-    .catch(error => showAlert(error));
-}
-
-window.onload = function onload() {
-  CarregaProdutos(); // primeira função a ser feita,vai bucar os dados
-}
-
-
 const inserirItemHtml = (produto) => {
   const secao = document.querySelector('.items');
   secao.appendChild(createProductItemElement(produto));
 };
-
-const showAlert = (message) => {
-  window.alert(message);
-};
-
-function createProductImageElement(imageSource) {
-  const img = document.createElement('img');
-  img.className = 'item__image';
-  img.src = imageSource;
-  return img;
-}
 
 function createCustomElement(element, className, innerText) {
   const e = document.createElement(element);
@@ -52,14 +13,52 @@ function createCustomElement(element, className, innerText) {
 function createProductItemElement({ sku, name, image }) {
   const section = document.createElement('section');
   section.className = 'item';
-
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-
   return section;
 }
+
+const converteObjetosDesejados = (objetosDoResult) => {
+  console.log(objetosDoResult);
+  objetosDoResult.map((item) => {
+    const cadaProduto = {
+      sku: item.id,
+      name: item.title,
+      image: item.thumbnail,
+    };
+    return inserirItemHtml(cadaProduto);
+  });
+}
+
+const showAlert = (message) => {
+  window.alert(message);
+};
+
+const CarregaProdutos = () => {
+  const endpoint = 'https://api.mercadolibre.com/sites/MLB/search?q=$computador';
+  // não posso transformar direto em json porque tem que esperar respostas do servidor
+  fetch(endpoint)
+    .then(resposta => resposta.json()) // endpoint convertido em json
+    .then((objeto) => { // dentro do objeto eu busquei os results
+      converteObjetosDesejados(objeto.results);
+    })
+    .catch(error => showAlert(error));
+}
+
+window.onload = function onload() {
+  CarregaProdutos(); // primeira função a ser feita,vai bucar os dados
+}
+
+function createProductImageElement(imageSource) {
+  const img = document.createElement('img');
+  img.className = 'item__image';
+  img.src = imageSource;
+  return img;
+}
+
+
 
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
