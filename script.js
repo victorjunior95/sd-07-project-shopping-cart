05e8ba3d0531e54c1cf59435b6039fc64bfc44d6
Sttefanie Maciel <macielsttefanie@gmail.com>
2020-10-27 15:58:08 -0300

    tentando

--- script.js
+++ script.js
@@ -52,6 +52,50 @@ const loadProducts = () => {
     });
   });
 };
+const addClickedItem = () => {
+  const buttonClicked = document.querySelector('.items');
+  buttonClicked.addEventListener('click', async (event) => {
+    const selected = event.target.closest('.item').dataset.id;
+    const API_URL_ITEM = `https://api.mercadolibre.com/items/${selected}`;
+    const siteResponse = await fetch(API_URL_ITEM).then(response => response.json());
+    const object =
+      {
+        sku: siteResponse.id,
+        name: siteResponse.title,
+        salePrice: siteResponse.price,
+      };
+    const createLi = createCartItemElement(object);
+    const listOl = document.querySelector('ol');
+    listOl.appendChild(createLi);
+  });
+};
+
+const fetchItemList = async () => {
+  const siteReturn = await fetch(API_URL_SEARCH)
+  .then(response => response.json())
+  .then(data => data.results);
+
+  siteReturn.forEach((element) => {
+    const obj = {
+      sku: element.id,
+      name: element.title,
+      image: element.thumbnail,
+    };
+    const addItem = document.querySelector('.items');
+    const a = createProductItemElement(obj);
+    addItem.appendChild(a);
+  });
+};
+
+const clearCart = () => {
+  const emptyCartButton = document.querySelector('.empty-cart');
+  emptyCartButton.addEventListener('click', () => {
+    document.querySelector('.cart__items').innerHTML = '';
+    localStorage.clear();
+  });
+};
+
+
 window.onload = function onload() {
   loadProducts();
 };
