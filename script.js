e3b421ffcb76a58cf32e38428925e841205c3773
Cosmo Rodrigues <cosmo.rsilva@gmail.com>
2020-10-26 22:55:44 -0300

    First commit

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-window.onload = function onload() { };
+function showError(message) {
+  return window.alert(message);
+}
 
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
@@ -14,10 +16,18 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
+function pageLoading() {
+  document.querySelector('.container').appendChild(
+    createCustomElement('h1', 'loading', 'loading...'));
+  setTimeout(() => {
+    document.querySelector('.container').removeChild(
+      document.querySelector('.loading'));
+  }, 300);
+}
+
 function createProductItemElement({ sku, name, image }) {
   const section = document.createElement('section');
   section.className = 'item';
-
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
@@ -26,18 +36,43 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
+// function getSkuFromProductItem(item) {
+//   return item.querySelector('span.item__sku').innerText;
+// }
+
+// function cartItemClickListener(event) {
+//   // coloque seu código aqui
+// }
+
+// function createCartItemElement({ sku, name, salePrice }) {
+//   const li = document.createElement('li');
+//   li.className = 'cart__item';
+//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+//   li.addEventListener('click', cartItemClickListener);
+//   return li;
+// }
+
+async function searchProducts(prdoduct) {
+  const items = document.querySelector('.items');
+  await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${prdoduct}`)
+    .then(result => result.json())
+    .then((object) => {
+      pageLoading();
+      object.results.forEach((element) => {
+        const { id: sku, title: name, thumbnail: image } = element;
+        const item = createProductItemElement({ sku, name, image });
+        items.appendChild(item);
+      });
+    })
+    .catch(erro => showError(erro));
 }
 
-function cartItemClickListener(event) {
-  // coloque seu código aqui
-}
+window.onchange = function () {
+  localStorage.setItem('saved-cart', document.querySelector('.cart').innerHTML);
+};
 
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
+window.onload = function onload() {
+  const prdoduct = 'computador';
+  searchProducts(prdoduct);
+  // document.querySelector('.cart').innerHTML = localStorage.getItem('saved-cart');
+};
