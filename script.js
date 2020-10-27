c8f59ffa8a6e54936c619116664f4c2ece756247
Emerson Nogueira Junqueira <emoonlit@pop-os.localdomain>
2020-10-27 19:11:12 -0300

    Solution 1

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
-window.onload = function onload() { };
+window.onload = function onload() { 
+  fetchProductsFromApi();
+  setupEventHandlers();
+};
 
+const setupEventHandlers = () => {
+  const searchButton = document.querySelector('.search_button');
+  searchButton.addEventListener('click', handlerSearchEvent);
+}
+
+const handlerSearchEvent = () => {
+  const currency = document.querySelector('#search_item').value;
+  cleanList();
+  return fetchProductsFromApi(currency);
+}
+
+const cleanList = () => {
+  const sectionList = document.querySelector('.items');
+  sectionList.innerHTML = '';
+}
+ 
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -40,4 +59,27 @@ function createCartItemElement({ sku, name, salePrice }) {
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
+}
+
+async function fetchProductsFromApi(currency = 'computador') {
+  const endpoint = `https://api.mercadolibre.com/sites/MLB/search?q=${currency}`;
+  const response = await fetch(endpoint);
+  const object = await response.json();
+  const itens = await object.results;
+  return loadItems(itens);
+}
+
+function iterateProduct(product) {
+  const { id: sku, title: name, thumbnail: image, price: salePrice } = product;
+  const avalibleProduct = { sku, name, image, salePrice };
+  return avalibleProduct;
+}
+
+function loadItems(itens) {
+  const itensList = itens;
+  itensList.forEach(product => {
+    const item = iterateProduct(product);
+    const listItens = document.querySelector('.items');
+    listItens.appendChild(createProductItemElement(item));
+  });
 }
