d8d212202affda7a98141a0ab883d77a0e7ef988
ericvini <ericcobain3@gmail.com>
2020-10-27 14:23:16 -0300

    Requisito 2

--- script.js
+++ script.js
@@ -13,7 +13,6 @@ function createCustomElement(element, className, innerText) {
   e.innerText = innerText;
   return e;
 }
-
 function createProductItemElement({ sku, name, image }) {
   const section = document.createElement('section');
   section.className = 'item';
@@ -26,22 +25,36 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
-// function getSkuFromProductItem(item) {
-// return item.querySelector('span.item__sku').innerText;
-// }
 
-// function cartItemClickListener(event) {
-// cartItems.splice(cartItems.indexOf(event.target), 1);
-// }
+function getSkuFromProductItem(item) {
+  return item.querySelector('span.item__sku').innerText;
+}
 
+function cartItemClickListener() {
 
-// function createCartItemElement({ sku, name, salePrice }) {
-// const li = document.createElement('li');
-// li.className = 'cart__item';
-// li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-// li.addEventListener('click', cartItemClickListener);
-// return li;
-// }
+}
+function createCartItemElement({ sku, name, salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
+}
+function addProductToShoppingCar() {
+  const itemID = getSkuFromProductItem(event.target.parentNode);
+  console.log(itemID);
+  fetch(`https://api.mercadolibre.com/items/${itemID}`)
+    .then(result => result.json()
+      .then(({ id, title, price }) => {
+        const object = {
+          sku: id,
+          name: title,
+          salePrice: price,
+        };
+        document.querySelector('.cart__items').appendChild(createCartItemElement(object));
+      }),
+    );
+}
 const addProductItemElement = (section, local) => document.querySelector(local)
   .appendChild(section);
 
@@ -55,6 +68,8 @@ function products(arrayProducts) {
     const section = createProductItemElement(objectProduct);
     addProductItemElement(section, 'section.items');
   });
+  document.querySelectorAll('.item__add').forEach(element => element
+    .addEventListener('click', addProductToShoppingCar));
 }
 const getListOfProducts = async () => {
   const url = 'https://api.mercadolibre.com/sites/MLB/search?q=$computador';
