cae22fe5876e56f4dfd3eeaa1737af76c5d0d0e9
Luise Rios <luisemiranda@gmail.com>
2020-10-27 19:54:44 -0300

    requisitos 2 e 3

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ function createCustomElement(element, className, innerText) {
   const e = document.createElement(element);
   e.className = className;
   e.innerText = innerText;
+
   return e;
 }
 
@@ -28,18 +29,40 @@ function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
 //   return item.querySelector('span.item__sku').innerText;
 // }
 
-// function cartItemClickListener(event) {
-//   // coloque seu código aqui
-// }
+function cartItemClickListener(event) {
+  const selectedItem = event.target;
+  const parent = document.querySelector('.cart__items');
+  parent.removeChild(selectedItem);
+}
 
-// function createCartItemElement({ sku, name, salePrice }) {
-//   const li = document.createElement('li');
-//   li.className = 'cart__item';
-//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-//   li.addEventListener('click', cartItemClickListener);
-//   return li;
-// }
+function createCartItemElement({ sku, name, salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
+}
+const fetchCartList = (event) => {
+  const itemParent = event.target.parentNode;
+  const itemId = itemParent.querySelector('.item__sku').innerText;
+  const endpoint = `https://api.mercadolibre.com/items/${itemId}`;
+  const cartItems = document.querySelector('.cart__items');
+  fetch(endpoint)
+    .then(response => response.json())
+    .then(({ id: sku, title: name, price: salePrice }) => {
+      const data = {
+        sku,
+        name,
+        salePrice,
+      };
+      cartItems.appendChild(createCartItemElement(data));
+    });
+};
 
+const addButton = () => {
+  const button = document.querySelectorAll('.item__add');
+  button.forEach(element => element.addEventListener('click', fetchCartList));
+};
 const fetchProductsList = async () => {
   const endpoint = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
   try {
@@ -56,6 +79,7 @@ const fetchProductsList = async () => {
   }
 };
 
-window.onload = function onload() {
-  fetchProductsList();
+window.onload = async function onload() {
+  await fetchProductsList();
+  addButton();
 };
