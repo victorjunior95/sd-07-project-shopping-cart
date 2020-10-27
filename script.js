887d261635d5e669727b0248ef71cf45cf5e5a43
André Horman de Medeiros Correia <tavernamoderna8000@gmail.com>
2020-10-27 19:04:51 -0300

    Requisito 05 e 06 finalizado - 27/10/2020

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function createProductImageElement(imageSource) {
-  const img = document.createElement("img");
-  img.className = "item__image";
+  const img = document.createElement('img');
+  img.className = 'item__image';
   img.src = imageSource;
   return img;
 }
@@ -13,7 +13,7 @@ function createCustomElement(element, className, innerText) {
 }
 
 const addSectionItems = (section) => {
-  const sectionItems = document.querySelector(".items");
+  const sectionItems = document.querySelector('.items');
   sectionItems.appendChild(section);
 };
 
@@ -42,83 +42,88 @@ const fetchApiShopping = (product) => {
 };
 
 // SOMANDO ITEMS DA LISTA
-// const sumPriceListItems = () => {
-//   let count = 0;
+const sumPriceListItems = (price) => {
+  const strong = document.querySelector('.total-price')
+  strong.innerText = `TOTAL R$: ${price}`
+};
 
-//   Object.keys(localStorage).forEach((key) => {
-//     const storage = JSON.parse(localStorage.getItem(key));
-//     count += storage[key].price;
-//     // console.log(count);
-//   });
-
-//   const p = document.createElement('p');
-//   p.classList.add('total-price');
-//   p.innerHTML = `<strong>Valor Total: R$${count}</strong>`;
-//   addLiCartItem(p);
-// };
+const updatePrice = () => {
+  let array = JSON.parse(localStorage.getItem('list'));
+  let value = 0;
+  // console.log(array);
+  array.forEach((li) => {
+    // console.log(li.split('$')[1]);
+    value += parseFloat(li.split('$')[1]);
+  });
+  sumPriceListItems(value);
+};
 
 // ADICIONANDO LOCAL STORAGE
 const addLocalStorage = (key, value) => {
   localStorage.setItem(key, JSON.stringify(value));
-  // sumPriceListItems();
 };
 
 // CRIANDO ARRAY NO LOCAL STORAGE
 const arrayLocalStorage = () => {
   const array = [];
-  const ol = document.querySelector(".cart__items");
+  const ol = document.querySelector('.cart__items');
   ol.childNodes.forEach((li) => array.push(li.innerText));
   return array;
-}
+};
 
 // REMOVENDO ITENS DO STORAGE
 function cartItemClickListener(event) {
   const parent = event.target.parentNode;
   // console.log(JSON.parse(localStorage.getItem('list')));
-  const array = JSON.parse(localStorage.getItem("list"));
+  const array = JSON.parse(localStorage.getItem('list'));
   // console.log(event.target.innerText)
   array.forEach((li) => {
     if (event.target.innerText === li) {
       parent.removeChild(event.target);
       localStorage.clear();
-      addLocalStorage("list", arrayLocalStorage());
+      addLocalStorage('list', arrayLocalStorage());
+      updatePrice();
     }
   });
 }
 
 // ADD LI NA OL
 const addLiCartItem = (li) => {
-  const ol = document.querySelector(".cart__items");
+  const ol = document.querySelector('.cart__items');
   ol.appendChild(li);
 };
 
 // ADD NO LOCAL STORAGE
 function createCartItemElement({ id: sku, title: name, price: salePrice }) {
-  const li = document.createElement("li");
-  li.className = "cart__item";
+  const li = document.createElement('li');
+  li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener("click", cartItemClickListener);
+  li.addEventListener('click', cartItemClickListener);
   addLiCartItem(li);
-  addLocalStorage("list", arrayLocalStorage());
+  addLocalStorage('list', arrayLocalStorage());
+  updatePrice();
 }
 
 // ATUALIZAR LOCAL STORAGE
 const updateLiStorage = () => {
   // console.log(JSON.parse(localStorage.getItem('list'))[1]);
-  let array = JSON.parse(localStorage.getItem("list"));
-  array.forEach((info) => {
-    // console.log(info)
-    const li = document.createElement("li");
-    li.className = "cart__item";
-    li.innerText = info;
-    li.addEventListener("click", cartItemClickListener);
-    addLiCartItem(li);
-  });
+  let array = JSON.parse(localStorage.getItem('list'));
+  if (array) {
+    array.forEach((info) => {
+      // console.log(info)
+      const li = document.createElement('li');
+      li.className = 'cart__item';
+      li.innerText = info;
+      li.addEventListener('click', cartItemClickListener);
+      addLiCartItem(li);
+    });
+    updatePrice();
+  }
 };
 
 // RESGATAR ID DOS ITENS
 function getSkuFromProductItem(item) {
-  return item.querySelector("span.item__sku").innerText;
+  return item.querySelector('span.item__sku').innerText;
 }
 
 // ADD ITEM DA LISTA
@@ -135,37 +140,40 @@ const fetchApiIds = (event) => {
 
 // CRIANDO PRODUTOS
 function createProductItemElement({ sku, name, image }) {
-  const section = document.createElement("section");
-  section.className = "item";
+  const section = document.createElement('section');
+  section.className = 'item';
 
-  section.appendChild(createCustomElement("span", "item__sku", sku));
-  section.appendChild(createCustomElement("span", "item__title", name));
+  section.appendChild(createCustomElement('span', 'item__sku', sku));
+  section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
   const button = createCustomElement(
-    "button",
-    "item__add",
-    "Adicionar ao carrinho!"
+    'button',
+    'item__add',
+    'Adicionar ao carrinho!'
   );
-  button.addEventListener("click", fetchApiIds);
+  button.addEventListener('click', fetchApiIds);
   section.appendChild(button);
   return section;
 }
 
 // LIMPANDO TODA LISTA
 const clearItems = () => {
-  const ol = document.querySelector(".cart__items");
-  ol.innerHTML = "";
+  const ol = document.querySelector('.cart__items');
+  const strong = document.querySelector('.total-price');
+  ol.innerHTML = '';
+  strong.innerHTML = '';
   localStorage.clear();
 };
 
 // EVENTO DE LIMPAR BOTÃO
 const clearShoppingCar = () => {
-  const button = document.querySelector(".empty-cart");
-  button.addEventListener("click", clearItems);
+  const button = document.querySelector('.empty-cart');
+  button.addEventListener('click', clearItems);
 };
 
 window.onload = function onload() {
-  fetchApiShopping("computador");
+  fetchApiShopping('computador');
   updateLiStorage();
   clearShoppingCar();
+  // sumPriceListItems(1);
 };
