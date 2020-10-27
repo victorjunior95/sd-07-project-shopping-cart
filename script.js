214cf391e1f8ab8e7e2edd4c19d1c24074433b9e
IsabelaCardoso <pcardoso.isabela@gmail.com>
2020-10-27 14:56:06 -0300

    refatoração com try/catch -  requisito 1

--- script.js
+++ script.js
@@ -24,19 +24,27 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 // forEach feito com a ajuda da resolução realizada pelo Vitor no fechamento do dia 26/10
-const fetchProducts = () => {
+const fetchProducts = async () => {
   const endpoint = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
-  fetch(endpoint)
-    .then(response => response.json())
-    .then((object) => {
-      const itemsElementHTML = document.querySelector('.items');
-      object.results.forEach((product) => {
-        const { id: sku, title: name, thumbnail: image } = product;
-        const item = createProductItemElement({ sku, name, image });
-        itemsElementHTML.appendChild(item);
-      });
-    });
-};
+
+  try {
+    const responseURL = await fetch(endpoint);
+    const object = await responseURL.json();
+    adaptJSONResponses(object); 
+  } catch (Error) {
+  alert(Error);
+  }
+}
+
+const adaptJSONResponses = (object) => {
+  const itemsElementHTML = document.querySelector('.items');
+  object.results.forEach((product) => {
+    const { id: sku, title: name, thumbnail: image } = product;
+    const item = createProductItemElement({ sku, name, image });
+    itemsElementHTML.appendChild(item);
+  });
+  buttonAddShopCart();
+}
 
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
@@ -52,6 +60,23 @@ function createCartItemElement({ sku, name, salePrice }) {
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
+}
+
+// add um evento ao botão 'Adicionar ao carrinho
+// realizar uma requisição para o endpoint 
+// "https://api.mercadolibre.com/items/$ItemID"
+// substituindo $ItemId pelo id selecionado. 
+// retornará um json com UM item
+// usar função acima para criar os componentes HTML
+// FILHO do cart_items
+
+const buttonAddShopCart = () => {
+  const allButtonsAddItem = document.getElementsByClassName('.item_add');
+  allButtonsAddItem.forEach((button) => { 
+    button.addEventListener('click', () => {
+      console.log("teste");
+    });
+  });
 }
 
 window.onload = function onload() {
