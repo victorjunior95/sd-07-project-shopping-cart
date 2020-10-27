c218d2e39a5a3a0da86b6bef81f604793477b698
Thays Costa <thayscosta3@gmail.com>
2020-10-27 01:49:34 -0300

    Requirement nº 1

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-window.onload = function onload() { };
-
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -26,6 +24,22 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
+const generateProducts = () => {
+  //Abstração facilitada pelo colega Vitor Rodrigues
+  const API_URL = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+  const itemsSection = document.querySelector('.items');
+  fetch(API_URL)
+  .then(response => response.json())
+  .then((item) => {
+    item.results.forEach((product) => {
+      const { id: sku, title: name, thumbnail: image } = product;
+      const eachItem = createProductItemElement({ sku, name, image });
+      itemsSection.appendChild(eachItem);
+    });
+  });
+};
+
+/*
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
@@ -41,3 +55,8 @@ function createCartItemElement({ sku, name, salePrice }) {
   li.addEventListener('click', cartItemClickListener);
   return li;
 }
+*/
+
+window.onload = function onload() {
+  generateProducts();
+};
