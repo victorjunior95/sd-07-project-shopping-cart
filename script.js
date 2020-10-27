df8abffc55eaf3d13a59600a905dfd460334939b
vladimirrchagas <vladimirrchagas@gmail.com>
2020-10-26 21:54:58 -0300

    Ajustando requisito 5

--- script.js
+++ script.js
@@ -15,7 +15,6 @@ function createCustomElement(element, className, innerText) {
 function createProductItemElement({ sku, name, image }) {
   const section = document.createElement('section');
   section.className = 'item';
-
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
