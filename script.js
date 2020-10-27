e921357e9c424ab36602a158d2bb74378e24c609
vitorAmorims <vtamorims@gmail.com>
2020-10-27 10:07:39 -0300

    code climate

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function createProductImageElement(imageSource) {
-  const img = document.createElement("img");
-  img.className = "item__image";
+  const img = document.createElement('img');
+  img.className = 'item__image';
   img.src = imageSource;
   return img;
 }
@@ -13,21 +13,21 @@ function createCustomElement(element, className, innerText) {
 }
 
 function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
-  const elements = document.querySelector(".items");
-  const section = document.createElement("section");
-  section.className = "item";
-  section.appendChild(createCustomElement("span", "item__sku", sku));
-  section.appendChild(createCustomElement("span", "item__title", name));
+  const elements = document.querySelector('.items');
+  const section = document.createElement('section');
+  section.className = 'item';
+  section.appendChild(createCustomElement('span', 'item__sku', sku));
+  section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
   section.appendChild(
-    createCustomElement("button", "item__add", "Adicionar ao carrinho!")
+    createCustomElement('button', 'item__add', 'Adicionar ao carrinho!')
   );
   return elements.appendChild(section);
 }
 
 // requisito 2 passo 1 capiturar ol
 const cathOl = (element) => {
-  const chart = document.querySelector(".cart__items");
+  const chart = document.querySelector('.cart__items');
   chart.appendChild(element);
 };
 
@@ -36,24 +36,24 @@ function cartItemClickListener(event) {
 }
 
 function createCartItemElement({ id: sku, title: name, price: salePrice }) {
-  const li = document.createElement("li");
-  li.className = "cart__item";
+  const li = document.createElement('li');
+  li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener("click", cartItemClickListener);
+  li.addEventListener('click', cartItemClickListener);
   li.id = sku;
   return li;
 }
 
 function loading() {
-  const Espan = document.createElement("span");
-  Espan.className = "loading";
-  Espan.innerHTML = "loading...";
-  const getContainerElement = document.querySelector(".items");
+  const Espan = document.createElement('span');
+  Espan.className = 'loading';
+  Espan.innerHTML = 'loading...';
+  const getContainerElement = document.querySelector('.items');
   getContainerElement.appendChild(Espan);
 }
 
 const eraseElement = () => {
-  const getContainerElement = document.querySelector(".items");
+  const getContainerElement = document.querySelector('.items');
   getContainerElement.removeChild(getContainerElement.firstChild);
 };
 
@@ -69,9 +69,9 @@ const fetchToChart = (sku) => {
 
 // requisito 2 passo 3
 const appendToChart = (item) => {
-  const createDisplay = document.querySelector(".items");
+  const createDisplay = document.querySelector('.items');
   createDisplay.appendChild(item);
-  item.addEventListener("click", (event) => {
+  item.addEventListener('click', (event) => {
     const getSku = event.currentTarget.firstChild.innerText;
     fetchToChart(getSku);
   });
@@ -82,7 +82,7 @@ const fetchProducts = () => {
   loading();
   setTimeout(() => {
     const endpoint =
-      "https://api.mercadolibre.com/sites/MLB/search?q=computador";
+      'https://api.mercadolibre.com/sites/MLB/search?q=computador';
     fetch(endpoint)
       .then((data) => data.json())
       .then((data) =>
@@ -95,14 +95,14 @@ const fetchProducts = () => {
 };
 
 function getSkuFromProductItem(item) {
-  return item.querySelector("span.item__sku").innerText;
+  return item.querySelector('span.item__sku').innerText;
 }
 
 function cleanToChart() {
-  const buttonCleanChart = document.querySelector(".empty-cart");
-  buttonCleanChart.addEventListener("click", () => {
-    const chart = document.querySelector(".cart__items");
-    chart.innerHTML = "";
+  const buttonCleanChart = document.querySelector('.empty-cart');
+  buttonCleanChart.addEventListener('click', () => {
+    const chart = document.querySelector('.cart__items');
+    chart.innerHTML = '';
   });
 }
 
