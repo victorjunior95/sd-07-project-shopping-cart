78e00c2e75f382533ce96353767d602fd39699f0
Mateus Leite Alves de Almeida <mateuslaalmeida@gmail.com>
2020-10-27 15:54:10 -0300

    Alteraçoes para o primeiro requisito p.2

--- script.js
+++ script.js
@@ -26,35 +26,34 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
-function getSkuFromProductItem(item) {
+/* function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
-}
+} */
 
-function cartItemClickListener(event) {
-  // coloque seu código aqui 
-}
+/* function cartItemClickListener(event) {
+  // coloque seu código aqui
+} */
 
-function createCartItemElement({ sku, name, salePrice }) {
+/* function createCartItemElement({ sku, name, salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
-}
+} */
 
+// Função elaborada com base na explicação de Vitor Rodrigues
 function loadProducts() {
-  const fetchPromise = new Promise((resolve, reject) => {
-    fetch("https://api.mercadolibre.com/sites/MLB/search?q=computer")
-      .then(response => response.json())
-      .then(data => {
-        const items = document.querySelector('.items')
-        data.results.forEach(product => {
-          const { id: sku, title: name, thumbnail: image } = product;
-          const item = createProductItemElement({ sku, name, image });
-          items.appendChild(item);
-        })
-      })
-  });
+  fetch('https://api.mercadolibre.com/sites/MLB/search?q=computer')
+    .then(response => response.json())
+    .then((data) => {
+      const items = document.querySelector('.items');
+      data.results.forEach((product) => {
+        const { id: sku, title: name, thumbnail: image } = product;
+        const item = createProductItemElement({ sku, name, image });
+        items.appendChild(item);
+      });
+    });
 }
 
 window.onload(loadProducts());
