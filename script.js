1a0ce938ed00aaf04f6a2ac155d955424d680bda
afonsofn <afonsoafn@gmail.com>
2020-10-28 10:21:49 -0300

    Requesito 2 e 3 CCfix3

--- script.js
+++ script.js
@@ -12,6 +12,14 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
+function createCartItemElement({ id: sku, title: name, price: salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
+}
+
 const addProductsOnCart = (id) => {
   const endpoint = `https://api.mercadolibre.com/items/${id}`;
 
@@ -44,14 +52,6 @@ function createProductItemElement({ sku, name, image }) {
 
 function cartItemClickListener(event) {
   event.target.remove();
-}
-
-function createCartItemElement({ id: sku, title: name, price: salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
 }
 
 const loadProducts = (search) => {
