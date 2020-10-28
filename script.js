d7c4a5f30d25a35f5b8620d7dff7701508de10ca
Nicholas Torres J. Vasconcelos <nicolastorresjatoba@gmail.com>
2020-10-28 20:02:01 -0300

    Criado carrinho de compras, exclusão de itens por click e salvar carrinho no localStorage. Código refatorado para atender code climate

--- script.js
+++ script.js
@@ -5,11 +5,46 @@ function createProductImageElement(imageSource) {
   return img;
 }
 
-function createCustomElement(element, className, innerText) {
+function createCustomElement(element, className, innerText, id) {
   const e = document.createElement(element);
   e.className = className;
   e.innerText = innerText;
+  if (element === 'button') {
+    e.id = id;
+  }
   return e;
+}
+
+function cartLocalStorage() {
+  const lista = document.querySelector('.cart__items').innerHTML;
+  localStorage.setItem('carrinho', lista);
+}
+
+function cartItemClickListener(event) {
+  event.target.remove();
+  cartLocalStorage();
+}
+
+function createCartItemElement({ id: sku, title: name, price: salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
+}
+
+function addListElement(item) {
+  document.querySelector('.cart__items')
+    .appendChild(createCartItemElement(item));
+  cartLocalStorage();
+}
+
+function idButtonEvent() {
+  fetch(`https://api.mercadolibre.com/items/${event.target.id}`)
+    .then(response => response.json())
+      .then((object) => {
+        addListElement(object);
+      });
 }
 
 function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
@@ -19,10 +54,12 @@ function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!', sku))
+    .addEventListener('click', idButtonEvent);
 
   return section;
 }
+
 
 function addItemList(item) {
   const listItem = document.querySelector('.items');
@@ -38,23 +75,18 @@ function getItemsShopping() {
         });
       });
 }
-
+/*
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
-
-function cartItemClickListener(event) {
-
-}
-
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
+*/
+function loadCartStorage() {
+  document.querySelector('.cart__items').innerHTML = localStorage.getItem('carrinho');
+  const items = document.querySelectorAll('.cart__item');
+  items.forEach(item => item.addEventListener('click', cartItemClickListener));
 }
 
 window.onload = function onload() {
   getItemsShopping();
+  loadCartStorage();
 };
