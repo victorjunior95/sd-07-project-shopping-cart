d2dd3af0a88d088c20bb2f618c968c19716a501f
Camila Valentim <camilaifpb@hotmail.com>
2020-10-28 14:57:53 -0300

    requisito 2

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-window.onload = function onload() { };
-
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -30,9 +28,60 @@ function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
-function cartItemClickListener(event) {
+const saveInLocalStorage = () => {
+  const ol = document.querySelector('.cart__items').innerHTML;
+  localStorage.setItem('cartList', JSON.stringify(ol));
 
+  const totalPrice = document.querySelector('.total-price').innerHTML;
+  localStorage.setItem('totalPrice', JSON.stringify(totalPrice));
+};
+
+const sumPrice = async () => {
+  const lis = document.querySelectorAll('.cart__item');
+  const resultElement = document.querySelector('.total-price');
+  const nodeListForArr = Array.from(lis);
+
+  const sum = nodeListForArr.reduce((total, li) => {
+    const liValue = li.innerText;
+    const arrOfValue = liValue.split('$');
+    return total + Number(arrOfValue[1]);
+  }, 0);
+
+  resultElement.innerText = sum;
+  // resultElement.innerText = sum.toFixed(2);
+
+  saveInLocalStorage();
+};
+
+function cartItemClickListener(event) {
+  const ol = document.querySelector('.cart__items');
+  const li = event.target;
+  ol.removeChild(li);
+
+  await sumPrice();
+  saveInLocalStorage();
 }
+// Thiago Pederzolli JSON
+
+const loadLocalStorage = () => {
+  let jsonList = localStorage.getItem('cartList');
+  jsonList = JSON.parse(jsonList);
+
+  const ol = document.querySelector('.cart__items');
+
+  ol.innerHTML = jsonList;
+
+  const lis = document.querySelectorAll('li');
+
+  lis.forEach(li => li.addEventListener('click', cartItemClickListener));
+
+  const totalPrice = document.querySelector('.total-price');
+
+  let sumResult = localStorage.getItem('totalPrice');
+  sumResult = JSON.parse(sumResult);
+
+  totalPrice.innerHTML = sumResult;
+};
 
 function createCartItemElement({ sku, name, salePrice }) {
   const li = document.createElement('li');
