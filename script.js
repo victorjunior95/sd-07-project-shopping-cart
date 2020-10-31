5d06663e204eb2cad8eecbb07d71630f70b4cd26
Iury Nathan <iurynathan@gmail.com>
2020-10-31 15:07:23 -0300

    requisito 1

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-window.onload = function onload() { };
-
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -14,13 +12,13 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement({ sku, name, image }) {
+function createProductItemElement({ id, title, thumbnail }) {
   const section = document.createElement('section');
   section.className = 'item';
 
-  section.appendChild(createCustomElement('span', 'item__sku', sku));
-  section.appendChild(createCustomElement('span', 'item__title', name));
-  section.appendChild(createProductImageElement(image));
+  section.appendChild(createCustomElement('span', 'item__sku', id));
+  section.appendChild(createCustomElement('span', 'item__title', title));
+  section.appendChild(createProductImageElement(thumbnail));
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
 
   return section;
@@ -41,3 +39,21 @@ function createCartItemElement({ sku, name, salePrice }) {
   li.addEventListener('click', cartItemClickListener);
   return li;
 }
+
+const itemsApi = (search) => {
+  fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${search}`)
+    .then((response) => {
+      response.json()
+        .then((data) => {
+          const sectionItems = document.getElementsByClassName('items')[0];
+          const product = data.results;
+          product.forEach(item => {
+            sectionItems.appendChild(createProductItemElement(item));
+          });
+        });
+      });
+ };
+
+
+
+window.onload = function onload() { itemsApi("computador") };
