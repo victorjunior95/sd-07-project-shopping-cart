2daaf04fad99e9a49bb5500ee0a3458def6688ca
Diego F Affonso <diegofernandesaffonso@gmail.com>
2020-11-02 14:08:56 -0300

    requisito 1

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-window.onload = function onload() { };
-
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -26,18 +24,43 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
-function getSkuFromProductItem(item) {
+/* function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
-}
+} */
 
-function cartItemClickListener(event) {
+/* function cartItemClickListener(event) {
   // coloque seu código aqui
-}
+} */
 
-function createCartItemElement({ sku, name, salePrice }) {
+/* function createCartItemElement({ sku, name, salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
-}
+} */
+
+const products = (product) => {
+  const productsKeys = Object.keys(product);
+  const sectionItems = document.querySelector('.items');
+
+  productsKeys.forEach((key) => {
+    const { id: sku, title: name, thumbnail: image } = product[key];
+    const item = createProductItemElement({ sku, name, image });
+    sectionItems.appendChild(item);
+  });
+};
+
+const fetchItens = () => {
+  const endpoint = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+
+  fetch(endpoint)
+    .then(response => response.json())
+    .then((object) => {
+      products(object.results);
+    });
+};
+
+window.onload = function onload() {
+  fetchItens();
+};
