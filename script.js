7c4176c2d3913cd06fc8f88a49e7beb3cec5aaa9
Lets <letsferreiratx@gmail.com>
2020-11-02 16:10:44 -0300

    adicionando items ao carrinho

--- script.js
+++ script.js
@@ -11,28 +11,6 @@ function createCustomElement(element, className, innerText) {
   e.innerText = innerText;
   return e;
 }
-
-function createProductItemElement({ sku, name, image }) {
-  const section = document.createElement('section');
-  section.className = 'item';
-
-  section.appendChild(createCustomElement('span', 'item__sku', sku));
-  section.appendChild(createCustomElement('span', 'item__title', name));
-  section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-
-  return section;
-}
-
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
-
-function cartItemClickListener(event) {
-  // coloque seu código aqui
-  // start
-}
-
 function createCartItemElement({ sku, name, salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
@@ -40,6 +18,42 @@ function createCartItemElement({ sku, name, salePrice }) {
   li.addEventListener('click', cartItemClickListener);
   return li;
 }
+const fetchAddCart = (id) => {
+  const endpoint = `https://api.mercadolibre.com/items/${id}`;
+  fetch(endpoint)
+  .then(response => response.json())
+  .then((data) => {
+    const ListItems = document.querySelector('ol.cart__items');
+    const { id: sku, title: name, price: salePrice } = data;
+    const listItem = createCartItemElement({ sku, name, salePrice });
+    ListItems.appendChild(listItem);
+  });
+};
+
+function getSkuFromProductItem(item) {
+  return item.querySelector('span.item__sku').innerText;
+}
+function createProductItemElement({ sku, name, image }) {
+  const section = document.createElement('section');
+  section.className = 'item';
+
+  section.appendChild(createCustomElement('span', 'item__sku', sku));
+  section.appendChild(createCustomElement('span', 'item__title', name));
+  section.appendChild(createProductImageElement(image));
+  const button = section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+
+  button.addEventListener('click', () => {
+    const item = button.parentElement;
+    fetchAddCart(getSkuFromProductItem(item));
+  });
+  return section;
+}
+
+function cartItemClickListener(event) {
+  // coloque seu código aqui
+  // start
+}
+
 const loadProducts = () => {
   const endpoint = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
   fetch(endpoint)
