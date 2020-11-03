9f178463995e0e9f58a2067b3c326a0001c70398
v-icto-r <victor130496@gmail.com>
2020-11-02 23:23:06 -0300

    Reqquisito 02

--- script.js
+++ script.js
@@ -17,13 +17,21 @@ function createCustomElement(element, className, innerText) {
 function createProductItemElement({ sku, name, image }) {
   const section = document.createElement('section');
   section.className = 'item';
-
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-
+  const button = createCustomElement('button', 'item__add', 'Adicionar ao carrinho!');
+  button.addEventListener('click', ItemclickListener);
+  section.appendChild(button);
   return section;
+}
+
+function renderProducts(arrayProducts) {
+  arrayProducts.forEach((product) => {
+    const section = document.querySelector('.items');
+    const itemProduct = createProductItemElement(product);
+    section.appendChild(itemProduct);
+  });
 }
 
 function getSkuFromProductItem(item) {
@@ -34,13 +42,30 @@ function cartItemClickListener(event) {
   // coloque seu código aqui
 }
 
-function createCartItemElement({ sku, name, salePrice }) {
+  // Resolução com ajuda na turma 6, requisito 2
+function includeItemcart(item) {
+  const list = document.querySelector('.cart__items');
+  list.appendChild(item);
+}
+
+function createCartItemElement({ id: sku, title: name, price: salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
   return li;
 }
+
+function ItemclickListener(event) {
+  const id = event.target.parentNode.firstChild.innerText;
+  const endpoint = `https://api.mercadolibre.com/items/${id}`;
+  fetch(endpoint)
+    .then(response => response.json())
+    .then((object) => {
+      const item = createCartItemElement(object);
+      includeItemcart(item);
+    });
+}
+
 // Aula 9.4 requisito 1
 const loadProducts = () => {
   const endpoint = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
