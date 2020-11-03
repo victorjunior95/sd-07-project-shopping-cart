e2a548095e6cc978c014d8b2734f51371cd509b3
afonsofn <afonsoafn@gmail.com>
2020-11-03 10:26:14 -0300

    requesito Local Storage

--- script.js
+++ script.js
@@ -87,7 +87,26 @@ const loadProducts = (search) => {
     });
 };
 
+const addLocalStorage = () => {
+  const object = [];
+  const li = document.querySelectorAll('.cart__item');
+  li.forEach(courrentItem => object.push(courrentItem.innerText));
+  localStorage.setItem("shopping cart", JSON.stringify(object));
+}
+
+const loadLocalStorage = () => {
+  const ol = document.querySelector('.cart__items');
+  const storageItems = JSON.parse(localStorage.getItem("shopping cart"));
+  if (storageItems) {
+    storageItems.forEach((item) => {
+      const li = createCustomElement('li', 'cart__item', item);
+      ol.appendChild(li);
+    });
+  }
+}
+
 window.onload = function onload() {
   loadProducts('computador');
   cleanCart();
+  loadLocalStorage();
 };
