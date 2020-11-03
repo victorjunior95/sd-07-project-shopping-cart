e02b0762ebc3871906958379752ad2881dfaa116
alexandrefaustino <alexandrefaculdade@gmail.com>
2020-11-03 12:49:45 -0300

    Commit 0.1.9

--- script.js
+++ script.js
@@ -115,7 +115,7 @@ function totalCartPrice(data) {
   const spanPrice = document.querySelector('.total-price');
   const { price } = data;
   totalPrice += price;
-  spanPrice.innerText += totalPrice;
+  spanPrice.innerText = totalPrice;
 }
 
 async function addProductToCart(id) {
