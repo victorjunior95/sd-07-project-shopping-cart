5c2da3cb07a6e428956dd93529be5f42a4309c5c
Alstent <rafaelasltent@gmail.com>
2020-11-05 23:24:46 +0100

    resolvendo code climate

--- script.js
+++ script.js
@@ -69,7 +69,7 @@ function removeLoadingTxt() {
 function createCartItemElement({ sku, name, salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: R$${salePrice}`;
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
 }
