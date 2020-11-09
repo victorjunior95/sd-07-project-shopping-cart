8a6ef370677056c3a1d870e45d52afaa387ec7f9
Leonnardo Lair <leonnardo0316@gmail.com>
2020-11-09 20:03:59 -0300

    second try requisite 01

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-window.onload = function onload() {
-  FetchSearch("computador");
-};
 
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
@@ -16,7 +13,7 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement( sku, name, image ) {
+function createProductItemElement(sku, name, image) {
   const section = document.createElement('section');
   section.className = 'item';
 
@@ -28,33 +25,36 @@ function createProductItemElement( sku, name, image ) {
   return section;
 }
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
+// function getSkuFromProductItem(item) {
+//   return item.querySelector('span.item__sku').innerText;
+// }
 
-function cartItemClickListener(event) {
-  // coloque seu código aqui
-}
+// function cartItemClickListener(event) {
+//   // coloque seu código aqui
+// }
 
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
+// function createCartItemElement({ sku, name, salePrice }) {
+//   const li = document.createElement('li');
+//   li.className = 'cart__item';
+//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+//   li.addEventListener('click', cartItemClickListener);
+//   return li;
+// }
 
-const FetchSearch = product => {
-  const productApi = `https://api.mercadolibre.com/sites/MLB/search?q=${product}`
+function FetchSearch(product) {
+  const productApi = `https://api.mercadolibre.com/sites/MLB/search?q=${product}`;
   fetch(productApi)
-    .then(response => {
-      response.json()
-      .then(data => {data.results.forEach(singleProduct => {
-      console.log( `${singleProduct.thumbnail}|${singleProduct.title}|${singleProduct.id}`);
-      document.querySelector(".items").appendChild(
-        createProductItemElement(singleProduct.id, singleProduct.title, singleProduct.thumbnail)
-      );
+    .then((response) => {
+      response.json().then((data) => {
+        data.results.forEach((singPro) => {
+          document.querySelector('.items')
+            .appendChild(createProductItemElement(singPro.id, singPro.title, singPro.thumbnail));
+        });
       });
-      })
     });
-  }
+}
+
+
+window.onload = function onload() {
+  FetchSearch('computador');
+};
