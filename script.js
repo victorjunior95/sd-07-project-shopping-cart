9c35057e3485b5d1284a3a2b70974d47d448a6dc
adrianoforcellini <adrianoforcellinipedretti@gmail.com>
2020-11-10 09:59:16 -0300

    finaly down

--- script.js
+++ script.js
@@ -26,16 +26,22 @@ function calculateValue(newList) {
   return new Promise((resolve) => {
     const newArray = [];
     newList.forEach(item => newArray.push(item.salePrice));
+// cada valor de cada item de newList e adicionado a newArray 
     const value = (newArray.reduce((acc, nextValue) => acc + nextValue, 0));
     resolve(value);
+// o valor dos itens sao somados e adicionados a variavel value
   });
 }
+// Uso de promisse constructor:
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/Promise
 
 async function sumTotalCart() {
   const newList = JSON.parse(localStorage.getItem('Carrinho de Compras'));
+// ao carregar a pagina o carrinho deve ser carregado de localStorage
   const divPrice = document.querySelector('.total-price');
   const divPriceChild = document.querySelector('.total-price-child');
   const value = await calculateValue(newList);
+// calculateValue e uma funçao assincrona devido ao uso do construtor new Promisse
   if (!divPriceChild) {
     const totalPriceInCart = createCustomElement('div', 'total-price-child bg-dark text-light', value);
     divPrice.appendChild(totalPriceInCart);
@@ -45,6 +51,8 @@ async function sumTotalCart() {
     divPrice.appendChild(totalPriceInCart);
   }
 }
+// Uso de JSON.parse, muito similar a Object.keys(so que JSON e uma string que representa um obj)
+// https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/JSON/parse
 
 function saveCart(newItemCart) {
   const arrayOfData = JSON.parse(localStorage.getItem('Carrinho de Compras'));
@@ -52,18 +60,29 @@ function saveCart(newItemCart) {
   localStorage.setItem('Carrinho de Compras', JSON.stringify(arrayOfData));
   sumTotalCart();
 }
+// localStorage.getItem acha o valor de uma chave pelo seu nome
+// https://developer.mozilla.org/pt-BR/docs/Web/API/Storage/getItem
+// localStorage.setItem : quando passado 'chave' e 'valor', irá adicionar esta chave ao storage,
+// ou atualizar o valor caso a chave já exista.
+// https://developer.mozilla.org/pt-BR/docs/Web/API/Storage/setItem
 
 function cartItemClickListener(event) {
   const arrayLocalStorage = JSON.parse(localStorage.getItem('Carrinho de Compras')) || [];
   const itemParent = document.querySelector('.cart__items');
   const arrayOfData = arrayLocalStorage.map(item => item.sku);
+// transforma cada item do array em item.sku
   const itemSelected = event.target;
   const correspondentIndex = arrayOfData.indexOf(itemSelected.dataset.sku);
+// o metodo indexOF retorna o primeiro índice em que o elemento pode ser encontrado no array
   arrayLocalStorage.splice(correspondentIndex, 1);
+// remove um elemento da lista a partir do indice correspondentIndex
+// https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/splice
   itemParent.removeChild(itemSelected);
   localStorage.clear();
   localStorage.setItem('Carrinho de Compras', JSON.stringify(arrayLocalStorage));
   sumTotalCart();
+// Uso de JSON.stringify (converte um valor numa string JSON)
+// https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/JSON/stringify
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
@@ -74,6 +93,10 @@ function createCartItemElement({ sku, name, salePrice }) {
   li.dataset.sku = sku;
   return li;
 }
+// A propriedade HTMLElement.dataset permite o acesso, em modo de leitura e escrita, a todos os atributos
+// de dados personalizado (data-*) no elemento. Ele é um mapa de DOMString, com uma entrada para cada
+// atributo de dados personalizado.
+// https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLElement/dataset
 
 function createCartAllElements(arrayOfData) {
   const list = document.querySelector('.cart__items');
@@ -170,3 +193,6 @@ window.onload = async function onload() {
   getClickForCart();
   loadClearButton();
 };
+
+// consultei o repositorio de Jessica de Paula para implementaçao das funçoes acima.
+// https://github.com/tryber/sd-07-project-shopping-cart/pull/93
