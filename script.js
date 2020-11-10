20c992479428f1687036bb748a8a0b495b2ad9fe
Leonnardo Lair <leonnardo0316@gmail.com>
2020-11-09 23:21:57 -0300

    requisite 03 first try

--- script.js
+++ script.js
@@ -6,15 +6,19 @@ function createProductImageElement(imageSource) {
   return img;
 }
 
-function cartItemClickListener(event) {
-  document.querySelector
+function cartItemClickListener(sku) {
+  const selfRemove = sku;
+  selfRemove.parentNode.removeChild(selfRemove);
 }
 
 function createCartItemElement(sku, name, salePrice) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener(sku));
+  li.id = `${sku}`;
+  li.addEventListener('click', () => {
+    cartItemClickListener(li);
+  });
   return li;
 }
 
@@ -50,7 +54,7 @@ function createProductItemElement(sku, name, image) {
 }
 
 // function getSkuFromProductItem(item) {
-//   return item.querySelector('span.item__sku').innerText;  
+//   return item.querySelector('span.item__sku').innerText;
 // }
 
 function fetchSearch(product) {
