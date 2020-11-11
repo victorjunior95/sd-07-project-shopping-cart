8c5f63603089ca8747796acddbb671bb077f427f
Arthur Nunes <coletivosuburbano@gmail.com>
2020-11-11 13:27:03 -0300

    refatorando

--- script.js
+++ script.js
@@ -1,42 +1,28 @@
-function createProductImageElement(imageSource) {
-  const img = document.createElement('img');
-  img.className = 'item__image';
-  img.src = imageSource;
-  return img;
-}
 
-function createCustomElement(element, className, innerText) {
-  const e = document.createElement(element);
-  e.className = className;
-  e.innerText = innerText;
-  return e;
-}
-
-const deleteLoading = () => {
-  const container = document.querySelector('.container');
-  const loadingMsg = container.children[0];
-  container.removeChild(loadingMsg);
-};
+let deleteLoading;
+let itemPrice;
+let totalPrice = 0;
 
 const showAlert = message => window.alert(message);
 
-const setupApi = async (endpoint, callback) => {
-  try {
-    const response = await fetch(endpoint);
-    const object = await response.json();
-    deleteLoading();
-    if (object.error) {
-      throw new Error(object.error);
-    } else {
-      callback(object);
-    }
-  } catch (error) {
-    showAlert(error);
-  }
-};
+document.addEventListener('DOMContentLoaded', () => {
+  const emptyButton = document.querySelector('.empty-cart');
+  const cartItems = document.querySelector('.cart__items');
+  emptyButton.addEventListener('click', () => {
+    cartItems.innerHTML = '';
+    localStorage.clear();
+    const totalPriceSpan = document.querySelector('.total-price');
+    totalPrice = 0;
+    totalPriceSpan.innerText = totalPrice.toFixed(0);
+  });
 
-let itemPrice;
-let totalPrice = 0;
+  deleteLoading = () => {
+    const container = document.querySelector('.container');
+    const loadingMsg = container.children[0];
+    container.removeChild(loadingMsg);
+  };
+  return deleteLoading;
+});
 
 function calculateTotal(price = 0, op = 0) {
   const totalPriceSpan = document.querySelector('.total-price');
@@ -45,7 +31,7 @@ function calculateTotal(price = 0, op = 0) {
   } else {
     totalPrice += price;
   }
-  totalPriceSpan.innerText = totalPrice;
+  totalPriceSpan.innerText = totalPrice.toFixed(0);
 }
 
 function cartItemClickListener(event) {
@@ -66,9 +52,7 @@ function createCartItemElement({ sku, name, salePrice }) {
 
 const printItemInCart = (object) => {
   const cartItems = document.querySelector('.cart__items');
-  const id = object.id;
-  const title = object.title;
-  const price = object.price;
+  const { id, title, price } = object;
   itemPrice = price;
 
   cartItems.appendChild(
@@ -79,6 +63,35 @@ const printItemInCart = (object) => {
     }),
   );
   calculateTotal(itemPrice);
+};
+
+function createCustomElement(element, className, innerText) {
+  const e = document.createElement(element);
+  e.className = className;
+  e.innerText = innerText;
+  return e;
+}
+
+function createProductImageElement(imageSource) {
+  const img = document.createElement('img');
+  img.className = 'item__image';
+  img.src = imageSource;
+  return img;
+}
+
+const setupApi = async (endpoint, callback, firstDelete) => {
+  try {
+    const response = await fetch(endpoint);
+    const object = await response.json();
+    if (firstDelete) firstDelete();
+    if (object.error) {
+      throw new Error(object.error);
+    } else {
+      callback(object);
+    }
+  } catch (error) {
+    showAlert(error);
+  }
 };
 
 function createProductItemElement({ sku, name, image }) {
@@ -113,19 +126,7 @@ const printItem = (object) => {
   );
 };
 
-document.addEventListener('DOMContentLoaded', () => {
-  const emptyButton = document.querySelector('.empty-cart');
-  const cartItems = document.querySelector('.cart__items');
-  emptyButton.addEventListener('click', () => {
-    cartItems.innerHTML = '';
-    localStorage.clear();
-    const totalPriceSpan = document.querySelector('.total-price');
-    totalPrice = 0;
-    totalPriceSpan.innerText = totalPrice
-  });
-});
-
 window.onload = async function onload() {
   const endpoint = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
-  await setupApi(endpoint, printItem);
+  await setupApi(endpoint, printItem, deleteLoading);
 };
