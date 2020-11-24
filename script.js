fc50fe53caa68d46975fae5b50b597ee8acf0683
Leonardo <lecastro16@gmail.com>
2020-11-23 23:06:13 -0300

    exercicios 1 ao 4 v4

--- script.js
+++ script.js
@@ -181,9 +181,9 @@ loadProducts = async () => {
 function clearCart() {
 //  const li = document.querySelectorAll('li.cart__item');
 //  li.addEventListener('click', cartItemClickListener);
-    document.querySelector('.empty-cart').addEventListener('click', () => {
-    document.querySelector('.cart__items').innerText = '';
-  });
+  document.querySelector('.empty-cart').addEventListener('click', () => {
+  document.querySelector('.cart__items').innerText = '';
+});
 }
 
 window.onload = function onload() {
