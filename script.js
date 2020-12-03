550af085f79b5af2b48564ea4dfdb87e056928f8
Victor Junior <victorjr.muchacho@gmail.com>
2020-12-03 18:59:36 -0300

    Requisito 1 - listar os produtos da API, CC analyzed

--- script.js
+++ script.js
@@ -24,26 +24,26 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
+// function getSkuFromProductItem(item) {
+//   return item.querySelector('span.item__sku').innerText;
+// }
 
-function cartItemClickListener(event) {
-  // coloque seu código aqui
-}
+// function cartItemClickListener(event) {
+//   // coloque seu código aqui
+// }
 
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
+// function createCartItemElement({ sku, name, salePrice }) {
+//   const li = document.createElement('li');
+//   li.className = 'cart__item';
+//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+//   li.addEventListener('click', cartItemClickListener);
+//   return li;
+// }
 
 const loadProducts = () => {
-  const endpoint = "https://api.mercadolibre.com/sites/MLB/search?q=computador";
+  const endpoint = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
   fetch(endpoint)
-    .then((response) => response.json())
+    .then(response => response.json())
     .then((objects) => {
       // console.log(objects.results);  // array de objects(products)
       const items = document.querySelector('.items');
@@ -53,10 +53,10 @@ const loadProducts = () => {
         // console.log(product);
         const item = createProductItemElement({ sku, name, image });
         items.appendChild(item);
-      })
+      });
     });
-}
+};
 
 window.onload = function onload() {
-  loadProducts()
- };
+  loadProducts();
+};
