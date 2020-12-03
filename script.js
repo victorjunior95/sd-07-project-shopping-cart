8d745ab00af34e1322898491ed1eea08ea79e1a0
Victor Junior <victorjr.muchacho@gmail.com>
2020-12-03 18:48:50 -0300

    Requisito 1 - listar os produtos da API

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-window.onload = function onload() { };
-
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -41,3 +39,24 @@ function createCartItemElement({ sku, name, salePrice }) {
   li.addEventListener('click', cartItemClickListener);
   return li;
 }
+
+const loadProducts = () => {
+  const endpoint = "https://api.mercadolibre.com/sites/MLB/search?q=computador";
+  fetch(endpoint)
+    .then((response) => response.json())
+    .then((objects) => {
+      // console.log(objects.results);  // array de objects(products)
+      const items = document.querySelector('.items');
+      const productDetails = objects.results;
+      productDetails.forEach((product) => {
+        const { id: sku, title: name, thumbnail: image } = product; // renomeando a chave
+        // console.log(product);
+        const item = createProductItemElement({ sku, name, image });
+        items.appendChild(item);
+      })
+    });
+}
+
+window.onload = function onload() {
+  loadProducts()
+ };
