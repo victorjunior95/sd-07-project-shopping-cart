81ac4fa5853ba14dcc571119e27a85d4b500fd38
dhiegodias <67385608+dhiegodias@users.noreply.github.com>
2020-12-02 21:29:09 -0300

    Projeto, venha CC

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-window.onload = function onload() { };
-
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -14,24 +12,25 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement({ sku, name, image }) {
-  const section = document.createElement('section');
-  section.className = 'item';
-
-  section.appendChild(createCustomElement('span', 'item__sku', sku));
-  section.appendChild(createCustomElement('span', 'item__title', name));
-  section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-
-  return section;
-}
-
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
+async function totalCart() {
+  const promise = new Promise (function(resolve, reject) {
+    const cart = document.querySelector('.cart__items').childNodes;
+    let sum = 0;
+    cart.forEach((item) => {
+      const priceValue = parseFloat(item.innerHTML.split('$')[1]);
+      sum += priceValue;
+    });
+    resolve(sum);
+  } );
+  const totalPrice = document.querySelector('.total-price');
+  totalPrice.innerHTML = await promise;
 }
 
 function cartItemClickListener(event) {
-  // coloque seu código aqui
+  event.target.remove();
+  const currentCart = document.querySelector('.cart__items');
+  localStorage.setItem('cart', currentCart.innerHTML);
+  totalCart();
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
@@ -42,3 +41,68 @@ function createCartItemElement({ sku, name, salePrice }) {
   return li;
 }
 
+function createProductItemElement({ sku, name, image }) {
+  const section = document.createElement('section');
+  section.className = 'item';
+  const addButtonCart = createCustomElement('button', 'item__add', 'Adicionar ao carrinho!');
+  const link = `https://api.mercadolibre.com/items/${sku}`;
+  addButtonCart.addEventListener('click', () => {
+    fetch(link)
+      .then(response => response.json())
+      .then((obj) => {
+        const { id, title, price } = obj;
+        const cartObj = createCartItemElement({ sku: id, name: title, salePrice: price });
+        const cartItems = document.querySelector('.cart__items');
+        cartItems.appendChild(cartObj);
+        localStorage.setItem('cart', cartItems.innerHTML);
+        totalCart()
+      });
+  });
+  section.appendChild(createCustomElement('span', 'item__sku', sku));
+  section.appendChild(createCustomElement('span', 'item__title', name));
+  section.appendChild(createProductImageElement(image));
+  section.appendChild(addButtonCart);
+
+  return section;
+}
+
+function loadCartLocal() {
+  const addLocalStorage = document.querySelector('.cart__items');
+  addLocalStorage.innerHTML = localStorage.getItem('cart');
+}
+
+function getSkuFromProductItem(item) {
+  return item.querySelector('span.item__sku').innerText;
+}
+
+const products = () => {
+  const endpoint = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+  const loading = document.querySelector('.loading');
+  loading.classList.add('active');
+  loading.classList.remove('inactive');
+  fetch(endpoint)
+    .then(response => response.json()).then((data) => {
+      const items = document.querySelector('.items');
+      data.results.forEach((product) => {
+        const { id: sku, title: name, thumbnail: image } = product;
+        const item = createProductItemElement({ sku, name, image });
+        items.appendChild(item);
+      });
+      loading.classList.remove('active');
+      loading.classList.add('inactive');
+    });
+};
+
+const emptyCart = () => {
+  document.querySelector('.empty-cart').addEventListener('click', () => {
+    document.querySelector('.cart__items').innerText = '';
+    localStorage.clear();
+  });
+};
+
+window.onload = function onload() {
+  products();
+  loadCartLocal();
+  totalCart();
+  emptyCart();
+};
