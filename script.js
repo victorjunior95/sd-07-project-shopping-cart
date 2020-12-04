ce2984ea4801d25cda8bed4fd4bf295d28d57f3e
Geziel <geziel.fernandez@pucp.pe>
2020-12-04 11:27:28 -0300

    novos requisitos

--- script.js
+++ script.js
@@ -12,6 +12,43 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
+const sentItemstoLocalStorage = () => {
+  const sentList = document.querySelector('.cart__items').innerHTML;
+  localStorage.setItem('cart_products', sentList);
+};
+
+function cartItemClickListener(event) {
+  // coloque seu código aqui,
+  event.target.remove();
+  sentItemstoLocalStorage();
+}
+
+const retrievingLocalStorage = () => {
+  const retrieveList = localStorage.getItem('cart_products');
+  document.querySelector('.cart__items').innerHTML = retrieveList;
+  const cartItems = document.querySelector('.cart__items');
+  cartItems.addEventListener('click', cartItemClickListener);
+};
+
+function createCartItemElement({ sku, name, salePrice }) {
+  const list = document.createElement('li');
+  list.className = 'cart__item';
+  list.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  list.addEventListener('click', cartItemClickListener);
+  return list;
+}
+
+const itemToCart = (itemObject) => {
+  const itensInCart = document.querySelector('.cart__items');
+  const { id, title, price } = itemObject;
+  const good = {
+    sku: id,
+    name: title,
+    salePrice: price,
+  };
+  itensInCart.appendChild(createCartItemElement(good));
+  // totalPrice();
+};
 function createProductItemElement({ sku, name, image }) {
   const section = document.createElement('section');
   section.className = 'item';
@@ -19,13 +56,22 @@ function createProductItemElement({ sku, name, image }) {
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+  // section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
 
-  // const button = createCustomElement('button', 'item__add', 'Adicionar ao carrinho!');
+  const button = createCustomElement('button', 'item__add', 'Adicionar ao carrinho!');
+  button.addEventListener('click', (r) => {
+    const skuClicked = r.target.parentNode.firstChild.innerText;
 
+    fetch(`https://api.mercadolibre.com/items/${skuClicked}`)
+    .then(response => response.json())
+    .then(json => itemToCart(json))
+    .then(() => sentItemstoLocalStorage());
+  });
+
+  section.appendChild(button);
   return section;
 }
-
+createCustomElement();
 /* Elemento que é criado pela função createProductItemElement
   <section class='item'>
     <span class='item__sku'>ASDE123456</span>
@@ -39,26 +85,30 @@ function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
-function cartItemClickListener(event) {
-  // coloque seu código aqui
-}
-
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
-
-window.onload = async () => {
-  const endpoint = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+const firsLoading = async ($QUERY) => {
+  const endpoint = `https://api.mercadolibre.com/sites/MLB/search?q=${$QUERY}`;
   const data = await fetch(endpoint);
+  // const resultado = await data.json();
   const { results } = await data.json();
+  // console.log(results);
 
   const items = document.getElementsByClassName('items');
   results.forEach(({ id, title, thumbnail }) => {
     const section = createProductItemElement({ sku: id, name: title, image: thumbnail });
     items[0].appendChild(section);
   });
+};
+
+window.onload = async () => {
+  firsLoading('computador');
+  retrievingLocalStorage();
+  // const endpoint = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+  // const data = await fetch(endpoint);
+  // const { results } = await data.json();
+
+  // const items = document.getElementsByClassName('items');
+  // results.forEach(({ id, title, thumbnail }) => {
+  //   const section = createProductItemElement({ sku: id, name: title, image: thumbnail });
+  //   items[0].appendChild(section);
+  // });
 };
