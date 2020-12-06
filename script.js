72309d890002abd609cd75b680baba4599296ad6
Lucas Miranda <lucasmiranda@pop-os.localdomain>
2020-12-06 20:29:21 -0300

    requisito 1

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-window.onload = function onload() { };
-
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -26,19 +24,18 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
+const request = async () => {
+  const apiUrl = await fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador');
+  const jsonResults = await apiUrl.json();
+  const result = jsonResults.results;
+  result.forEach((products) => {
+    const { id, title, thumbnail } = products;
+    const product = { sku: id, name: title, image: thumbnail };
+    const itemsList = document.querySelector('.items');
+    itemsList.appendChild(createProductItemElement(product));
+  });
+};
 
-function cartItemClickListener(event) {
-  // coloque seu código aqui
-}
-
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
-;
+window.onload = function onload() {
+  request();
+};
